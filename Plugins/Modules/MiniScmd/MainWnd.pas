3052d13599d2accfe3813e3a191a28c9a8ce17e3
Billi Berserker <billiberserker@sharpe.gov>
2006-08-04 09:02:19 +0000

    * first MiniSCMD module commit * changed unit directories * changed host application and working directory path

---
+++ Plugins/Modules/MiniScmd/MainWnd.pas
@@ -0,0 +1,162 @@
+{
+Source Name: MainWnd.pas
+Description: MiniScmd Main Window
+Copyright (C) Martin Krämer <MartinKraemer@gmx.net>
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+Main SharpE Site
+http://www.Sharpe-Shell.org
+
+Recommended Environment
+ - Compiler : Delphi 2005 (Personal Edition)
+ - OS : Windows 2000 or higher
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+unit MainWnd;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, GR32_Image, SharpEBaseControls, SharpEButton,
+  SharpESkinManager, SharpEScheme, SharpESkin, ExtCtrls, SharpEProgressBar,
+  JvSimpleXML, SharpApi, Jclsysinfo, Menus, Math, SharpEEdit;
+
+
+type
+  TMainForm = class(TForm)
+    Background: TImage32;
+    MenuPopup: TPopupMenu;
+    Settings1: TMenuItem;
+    SharpESkinManager1: TSharpESkinManager;
+    edit: TSharpEEdit;
+    procedure editKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure Settings1Click(Sender: TObject);
+  protected
+  private
+    sWidth      : integer;
+    procedure WMUpdateBangs(var Msg : TMessage); message WM_SHARPEUPDATEACTIONS;
+    procedure WMSharpEBang(var Msg : TMessage);  message WM_SHARPEACTIONMESSAGE;
+  public
+    ModuleID : integer;
+    BarWnd : hWnd;
+    procedure LoadSettings;
+    procedure ReAlignComponents;
+  end;
+
+
+implementation
+
+uses SettingsWnd,
+     uSharpBarAPI;
+
+{$R *.dfm}
+
+procedure TMainForm.LoadSettings;
+var
+  item : TJvSimpleXMLElem;
+begin
+  SharpApi.RegisterActionEx(PChar('!FocusMiniScmd ('+inttostr(ModuleID)+')'),'Modules',self.Handle,1);
+
+  sWidth     := 100;
+
+  item := uSharpBarApi.GetModuleXMLItem(BarWnd, ModuleID);
+  if item <> nil then with item.Items do
+  begin
+    sWidth     := IntValue('Width',100);
+  end;
+
+  ReAlignComponents;
+end;
+
+procedure TMainForm.ReAlignComponents;
+var
+  FreeBarSpace : integer;
+  newWidth : integer;
+begin
+  self.Caption := '';
+  if sWidth<20 then sWidth := 20;
+
+  edit.Left := 4;
+
+  FreeBarSpace := GetFreeBarSpace(BarWnd) + self.Width;
+  if FreeBarSpace <0 then FreeBarSpace := 1;
+  newWidth := sWidth + 8;
+  if newWidth > FreeBarSpace then Width := FreeBarSpace
+     else Width := newWidth;
+
+  edit.Width := max(1,Width - 8);
+end;
+
+
+procedure TMainForm.Settings1Click(Sender: TObject);
+var
+  SettingsForm : TSettingsForm;
+  item : TJvSimpleXMLElem;
+begin
+  try
+    SettingsForm := TSettingsForm.Create(nil);
+    SettingsForm.tb_size.Position   := sWidth;
+
+    if SettingsForm.ShowModal = mrOk then
+    begin
+      sWidth      := SettingsForm.tb_size.Position;
+
+      item := uSharpBarApi.GetModuleXMLItem(BarWnd, ModuleID);
+      if item <> nil then with item.Items do
+      begin
+        clear;
+        Add('Width',sWidth);
+      end;
+      uSharpBarAPI.SaveXMLFile(BarWnd);
+    end;
+    ReAlignComponents;
+
+  finally
+    SettingsForm.Free;
+    SettingsForm := nil;
+    SendMessage(self.ParentWindow,WM_UPDATEBARWIDTH,0,0);
+  end;
+end;
+
+procedure TMainForm.editKeyUp(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if Key = VK_RETURN then
+  begin
+    SetFocus;
+    SharpApi.SharpExecute(edit.Edit.Text);
+    edit.Text := '';
+    edit.Edit.Text := '';
+  end;
+end;
+
+procedure TMainForm.WMUpdateBangs(var Msg : TMessage);
+begin
+  SharpApi.RegisterActionEx(PChar('!FocusMiniScmd ('+inttostr(ModuleID)+')'),'Modules',self.Handle,1);
+end;
+
+procedure TMainForm.WMSharpEBang(var Msg : TMessage);
+begin
+  case msg.LParam of
+    1: edit.SetFocus;
+  end;
+end;
+
+end.
---
+++ Plugins/Modules/MiniScmd/MiniScmd.dpr
@@ -0,0 +1,222 @@
+{
+Source Name: MiniScmd.dpr
+Description: SharpBar Module Main Project File
+Copyright (C) Martin Krämer <MartinKraemer@gmx.net>
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+Main SharpE Site
+http://www.Sharpe-Shell.org
+
+Recommended Environment
+ - Compiler : Delphi 2005 (Personal Edition)
+ - OS : Windows 2000 or higher
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+library MiniScmd;
+
+
+
+uses
+  Windows,
+  Controls,
+  Dialogs,
+  SysUtils,
+  Forms,
+  Classes,
+  Contnrs,
+  SharpESkinManager,
+  SharpEBar,
+  StdCtrls,
+  JvSimpleXML,
+  MainWnd in 'MainWnd.pas' {MainForm},
+  SettingsWnd in 'SettingsWnd.pas' {SettingsForm},
+  SharpAPI in '..\..\..\Common\Libraries\SharpAPI\SharpAPI.pas',
+  uSharpBarAPI in '..\..\..\Components\SharpBar\uSharpBarAPI.pas',
+  graphicsFX in '..\..\..\Common\Units\SharpFX\graphicsFX.pas',
+  MouseTimer in '..\..\..\Common\Units\MouseTimer\MouseTimer.pas';
+
+type
+  TModule = class
+            private
+              FForm : TForm;
+              FID   : integer;
+              FPos  : integer;
+              FBarWnd  : hWnd;
+            public
+              constructor Create(pID : integer; pParent : hwnd); reintroduce;
+              destructor Destroy; override;
+            published
+              property ID   : integer read FID;
+              property Pos  : integer read FPos write FPos;
+              property Form : TForm   read FForm;
+              property BarWnd : hWnd  read FBarWnd;
+            end;
+
+var
+  ModuleList : TObjectList;
+  MouseTimer : TMouseTimer;
+
+{$R *.res}
+
+function GetControlByHandle(AHandle: THandle): TWinControl;
+begin
+ Result := Pointer(GetProp( AHandle,
+                            PChar( Format( 'Delphi%8.8x',
+                                           [GetCurrentProcessID]))));
+end;
+
+constructor TModule.Create(pID : integer; pParent : hwnd);
+begin
+  inherited Create;
+  FID   := pID;
+  FBarWnd := pParent;
+  FForm := TMainForm.CreateParented(pParent);
+  FForm.BorderStyle := bsNone;
+  try
+    FForm.Height := GetBarPluginHeight(FBarWnd);
+  except
+  end;
+  FForm.ParentWindow := pParent;
+  MouseTimer.AddWinControl(TMainForm(FForm));
+  with FForm as TMainForm do
+  begin
+    ModuleID := pID;
+    BarWnd   := FBarWnd;
+    LoadSettings;
+    ReAlignComponents;
+    Show;
+  end;
+ // FForm.Parent := GetControlByHandle(pParent);
+end;
+
+destructor TModule.Destroy;
+begin
+  FForm.Free;
+  FForm := nil;
+  inherited Destroy;
+end;
+
+function CreateModule(ID : integer;
+                      parent : hwnd) : hwnd;
+var
+  temp : TModule;
+begin
+  try
+    if ModuleList = nil then
+       ModuleList := TObjectList.Create;
+
+    if MouseTimer = nil then
+       MouseTimer := TMouseTimer.Create;
+
+    temp := TModule.Create(ID,parent);
+    ModuleList.Add(temp);
+  except
+    result := 0;
+    exit;
+  end;
+  result := temp.Form.Handle;
+end;
+
+function CloseModule(ID : integer) : boolean;
+var
+  n : integer;
+begin
+  if ModuleList = nil then exit;
+
+  try
+    for n := 0 to ModuleList.Count - 1 do
+        if TModule(ModuleList.Items[n]).ID = ID then
+        begin
+          MouseTimer.RemoveWinControl(TModule(ModuleList.Items[n]).Form);
+          if MouseTimer.ControlList.Count = 0 then
+             FreeAndNil(MouseTimer);
+          ModuleList.Delete(n);
+          result := True;
+          exit;
+        end;
+  except
+    result := False;
+  end;
+end;
+
+procedure Refresh(ID : integer);
+var
+  n : integer;
+  temp : TModule;
+begin
+  for n := 0  to ModuleList.Count - 1 do
+      if TModule(ModuleList.Items[n]).ID = ID then
+      begin
+        temp := TModule(ModuleList.Items[n]);
+        TMainForm(temp.Form).ReAlignComponents;
+      end;
+end;
+
+procedure PosChanged(ID : integer);
+var
+  n : integer;
+  temp : TModule;
+begin
+  for n := 0  to ModuleList.Count - 1 do
+      if TModule(ModuleList.Items[n]).ID = ID then
+      begin
+        temp := TModule(ModuleList.Items[n]);
+        TMainForm(temp.Form).Background.Bitmap.SetSize(temp.Form.Width,temp.Form.Height);
+        uSharpBarAPI.PaintBarBackGround(temp.BarWnd,TMainForm(temp.Form).Background.Bitmap,Temp.Form);
+      end;
+end;
+
+procedure SkinChanged(ID : integer);
+var
+  n : integer;
+  temp : TModule;
+begin
+  for n := 0  to ModuleList.Count - 1 do
+      if TModule(ModuleList.Items[n]).ID = ID then
+      begin
+        temp := TModule(ModuleList.Items[n]);
+        temp.Form.Height := GetBarPluginHeight(temp.BarWnd);
+        TMainForm(temp.Form).ReAlignComponents;
+      end;
+end;
+
+procedure ShowSettingsWnd(ID : integer);
+var
+  n : integer;
+  temp : TModule;
+begin
+  for n := 0 to ModuleList.Count - 1 do
+      if TModule(ModuleList.Items[n]).ID = ID then
+      begin
+        temp := TModule(ModuleList.Items[n]);
+        TMainForm(temp.FForm).Settings1Click(TMainForm(temp.FForm).Settings1);
+      end;
+end;
+
+
+Exports
+  CreateModule,
+  CloseModule,
+  Poschanged,
+  Refresh,
+  SkinChanged,
+  ShowSettingsWnd;
+
+
+end.
