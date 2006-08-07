1189b20d85bcb420bc9bc0f4e01f230e82cb6e4b
Billi Berserker <billiberserker@sharpe.gov>
2006-08-07 15:10:17 +0000

    * added clean.bat * added script for creating a complete directory structure for the default output path

---
+++ Create Output Directory Structure.bat
@@ -0,0 +1,25 @@
+cd ..
+mkdir SharpE
+cd SharpE
+
+mkdir Center
+cd Center
+mkdir Root
+cd Root
+mkdir Plugins
+cd Plugins
+mkdir _Objects
+mkdir _Services
+cd..
+cd..
+cd..
+
+mkdir Modules
+
+mkdir Objects
+
+mkdir Services
+
+mkdir Settings
+
+Pause
