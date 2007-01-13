5a0c18a92900fa197cdd63b40294151bc0ccef07
Billi Berserker <billiberserker@sharpe.gov>
2007-01-13 16:57:35 +0000

    * changed to save skin as file stream (memory stream code removed)

--- Plugins/Services/Skin/uSkinServer.pas
+++ Plugins/Services/Skin/uSkinServer.pas
@@ -2,8 +2,7 @@
 Source Name: uSkinServer
 Description: SharpESkin service to provide all app with skin graphics
 Copyright (C) Malx (Malx@sharpe-shell.org)
-
-Common: SharpApi
+              Martin Krämer (MartinKraemer@gmx.net)
 
 Source Forge Site
 https://sourceforge.net/projects/sharpe/
@@ -46,38 +45,16 @@ Type
       procedure FormCreate(Sender: TObject);
     private
       FSkin      : TSharpESkin;
-      FSize      : Cardinal;
-      FBlockName : String;
 
       FFileName  : string;
-      FNrLoaded  : Integer;
-      FLastHwnd  : Cardinal;
-      FNrReq     : Integer;
-
-      FMapHandle : THandle;
-      FMem       : Pointer;
-      FStream    : TMemoryStream;
-
+      FStream    : TFileStream;
 
       procedure UpdateTheme(var Msg: TMessage); message WM_SHARPETHEMEUPDATE;
       procedure UpdateScheme(var Msg: TMessage); message WM_SHARPEUPDATESETTINGS;
-      procedure IncomingRequest(var Msg: TMessage); message WM_ASKFORSYSTEMSKIN;
-      procedure ReloadSharedMemory;
-      procedure AllocMemory;
-      procedure DeAllocMemory;
-      procedure PutListinMemory;
+      procedure UpdateStreamFile;
     { Private declarations }
     public
-      FTrayManager : TObject;
-      isClosing : boolean;
-      procedure UpdateInfoDlg;
     { Public declarations }
-    end;
-
-    TSharedSkinInfo = record
-      Version   : string[4]; //This one should NEVER move
-      BlockName : string[255];
-      BlockSize : Cardinal;
     end;
 
 {$R *.dfm}
@@ -85,154 +62,38 @@ Type
 implementation
 uses uInfodlg;
 
-function CalculateSize(skin : TSharpESkin): Cardinal;
-var i : integer;
-   totsize : cardinal;
-begin
-  totsize := 0;
-  //Add pixeldata
-  for i := 0 to skin.BitmapList.Count-1 do Begin
-    with skin.BitmapList.Items[i] do
-      inc(totsize,Bitmap.Height*Bitmap.Width*4);
-  end;
-  //Add bitmap headers
-  inc(totsize,skin.BitmapList.Count*(SizeOf(tagBITMAPINFO)+4));
-  //Add a bitmap count in the beginning
-  inc(totsize, 4);
-  result := totsize;
-end;
 
 procedure TSkinServer.FormCreate(Sender: TObject);
 begin
   FSkin := TSharpESkin.Create(self);
-  FStream := TMemoryStream.Create;
-  FNrLoaded := 0;
-  FFileName := '';
-  FLastHwnd := 0;
-  FNrReq := 0;
-  FSize := 0;
-  ReloadSharedMemory;
+  //FStream := TFileStream.Create(SharpApi.GetSharpeUserSettingsPath + 'SharpE.skin',fmCreate or fmShareDenyWrite);
+  UpdateStreamFile;
   SharpEBroadCast(WM_SYSTEMSKINUPDATE,0,0);
 end;
 
 procedure TSkinServer.FormDestroy(Sender: TObject);
 begin
-  DeAllocMemory;
-  FSkin.Free;
-  FStream.Free;
+  if FStream <> nil then FreeAndNil(FStream);
 end;
 
-procedure TSkinServer.UpdateInfoDlg;
+procedure TSkinServer.UpdateStreamFile;
+var
+  n : integer;
 begin
-  if InfoDlg <> nil then begin
-    InfoDlg.lSkinFile.caption := FFileName;
-    InfoDlg.lnr.caption       := inttostr(FNrLoaded);
-    InfoDlg.lNrReq.caption    := inttostr(FNrReq);
-    InfoDlg.lLastHwnd.caption := inttostr(application.handle);
-  end;
-end;
-
-procedure TSkinServer.ReloadSharedMemory;
-begin
-  //dealloc handles to old shared memory
-  DeAllocMemory;
   //Load new skin
   SharpThemeApi.LoadTheme(True);
-  FFileName := SharpThemeApi.GetSkinDirectory + '\Skin.xml';
+  FFileName := SharpThemeApi.GetSkinDirectory + 'Skin.xml';
   FSkin.LoadFromXmlFile(FFileName);
-  inc(FNrLoaded);
-  //Calculate size of bitmaplist
-  FSize := CalculateSize(FSkin);
-  //Save skin structure as stream (without bitmap data)
-  FSkin.SaveToStream(FStream,false);
-  //Add structure size to memory size
-  inc(FSize,FStream.Size+8);
-  //allocate enough memory
-  AllocMemory;
-  //Put all data to memory
-  PutListinMemory;
-  //Clear the temporary stream
-  FStream.Clear;
-  //Update info Dlg
-  UpdateInfoDlg;
-end;
-
-procedure TSkinServer.PutListinMemory;
-var
-  i : integer;
-  pos : cardinal;
-  source : pointer;
-  dest  : pointer;
-  co    : ^Cardinal;
-begin
-  if FMem <> nil then begin
-    with FSkin.BitmapList do begin
-      co  := FMem;
-      co^ := cardinal(Count);
-      pos := 12 + Count*4;
-      for i := 0 to Count - 1 do begin
-         co  := Pointer(Cardinal(FMem) + cardinal(i)*4 + 4);
-         co^ := pos;
-         with Items[i].Bitmap do begin
-           source := pointer(@BitmapInfo);
-           dest := pointer(cardinal(FMem) + pos);
-           System.Move(source^, dest^, sizeof(tagBITMAPINFO));
-           inc(pos, sizeof(tagBITMAPINFO));
-
-           source := pointer(Bits);
-           dest := pointer(cardinal(FMem) + pos);
-           System.Move(source^, dest^, width*Height*4);
-           inc(pos, Width*Height*4);
-         end;
-      end;
-      co  := Pointer(Cardinal(FMem) + cardinal(count)*4 + 4);
-      co^ := pos;
-      co  := Pointer(Cardinal(FMem) + cardinal(count)*4 + 8);
-      co^ := Cardinal(FStream.Size);
-      dest := pointer(cardinal(FMem) + pos);
-      FStream.Position := 0;
-      FStream.ReadBuffer(dest^, FStream.Size);
-    end;
+  if FStream <> nil then
+     FreeAndNil(FStream);
+  try
+    DeleteFile(SharpApi.GetSharpeUserSettingsPath + 'SharpE.skin');
+  finally
+    FStream := TFileStream.Create(SharpApi.GetSharpeUserSettingsPath + 'SharpE.skin',fmCreate);
   end;
+  FSkin.SaveToStream(FStream,true);
+  FStream.Free;
 end;
-
-procedure TSkinServer.AllocMemory;
-var
-  res : hResult;
-begin
-  res := ERROR_ALREADY_EXISTS;
-  while res = ERROR_ALREADY_EXISTS do begin
-    FBlockName := 'SharpESkin' + inttostr(random(20000));
-    FMapHandle := CreateFileMapping($FFFFFFFF, nil, PAGE_READWRITE,
-                               0, FSize, pchar(FBlockName));
-    if (FMapHandle = 0)
-      then raise Exception.Create('TSkinServer.AllocMemory filemap error');
-    res := GetLastError;
-  end;
-
-  FMem := MapViewOfFile(FMapHandle, FILE_MAP_WRITE, 0, 0, 0 );
-  if (FMem = nil )
-    then raise Exception.Create('TSkinServer.AllocMemory mapview error');
-
-end;
-
-Procedure TSkinServer.DeAllocMemory;
-begin
-  //This function deallocs skinserver handles to the shared memory
-  //BUT, as long as ANY client still has a valid handle to the memory
-  //it will still exist, and not be freed.
-  if (FMem <> nil) then begin
-    if not UnmapViewOfFile(FMem)
-       then raise Exception.Create( 'TSkinServer.DeAlloc unmapview error');
-       FMem := nil;
-  end;
-  if (FMapHandle <> 0) then
-    if not CloseHandle(FMapHandle)
-       then raise Exception.Create('TSkinServer.DeAlloc close handle error' );
-end;
-
-
-
 
 procedure TSkinServer.CreateParams(var Params: TCreateParams);
 begin
@@ -250,32 +111,14 @@ end;
 
 procedure TSkinServer.UpdateScheme(var Msg: TMessage);
 begin
-  ReloadSharedMemory;
+  UpdateStreamFile;
   SharpEBroadCast(WM_SYSTEMSKINUPDATE,0,0);
 end;
 
 procedure TSkinServer.UpdateTheme(var Msg: TMessage);
 begin
-  ReloadSharedMemory;
+  UpdateStreamFile;
   SharpEBroadCast(WM_SYSTEMSKINUPDATE,0,0);
-end;
-
-procedure TSkinServer.IncomingRequest(var Msg: TMessage);
-var ds : TCopyDataStruct;
-    ski : TSharedSkinInfo;
-begin
-  ds.dwData := 301;
-  ds.cbData := SizeOf(TSharedSkinInfo);
-  ski.Version := '1.0';
-  ski.BlockName := FBlockName;
-  ski.BlockSize := FSize;
-  ds.lpData := @ski;
-  SendMessage(msg.WParam,WM_COPYDATA ,0,Cardinal(@ds));
-
-  inc(FNrReq);
-  FLastHwnd := msg.WParam;
-  //Update info dlg
-  UpdateInfoDlg;
 end;
 
 end.
