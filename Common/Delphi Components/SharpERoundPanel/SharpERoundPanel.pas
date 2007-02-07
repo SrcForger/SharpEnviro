c70a8f2a413c0400a301b9eb997888aff2a136d0
Lee Pixol <pixol@sharpe.gov>
2007-02-07 12:34:00 +0000

    * Added Color Scheme for Border * Added option to have a border corner flat, mainly for SharpCenter * Some drawing bugs fixed

--- Common/Delphi Components/SharpERoundPanel/SharpERoundPanel.pas
+++ Common/Delphi Components/SharpERoundPanel/SharpERoundPanel.pas
@@ -10,7 +10,11 @@ type
   private
     FRoundValue: Integer;
     FBorderColor: TColor;
+    FBorder: Boolean;
+    FBackgroundColor: TColor;
     procedure SetBorderColor(const Value: TColor);
+    procedure SetBorder(const Value: Boolean);
+    procedure SetBackgroundColor(const Value: TColor);
   protected
     procedure Resize; override;
     procedure Paint; override;
@@ -20,6 +24,8 @@ type
   published
     property RoundValue: Integer read FRoundValue write SetRoundValue;
     property BorderColor: TColor read FBorderColor write SetBorderColor;
+    property Border: Boolean read FBorder write SetBorder;
+    property BackgroundColor : TColor read FBackgroundColor write SetBackgroundColor;
   end;
 
 procedure Register;
@@ -35,24 +41,62 @@ end;
 constructor TSharpERoundPanel.Create(AOwner: TComponent);
 begin
   Inherited;
-  FRoundValue := 8;
+  FRoundValue := 10;
+  ParentBackground := false;
+  FBackgroundColor := clBtnFace;
+  Color := clWhite;
+  FBorderColor := clBtnFace;
+
+  BevelInner := bvNone;
+  BevelOuter := bvNone;
 end;
 
 procedure TSharpERoundPanel.Paint;
 begin
-  Canvas.Pen.Color := FBorderColor;
-  Canvas.Brush.Color := Color;
+  Canvas.Brush.Color := FBackgroundColor;
   Canvas.FillRect(ClientRect);
+
+  if FBorder then
+  Canvas.Pen.Color := FBorderColor else
+  Canvas.Pen.Color := Color;
+
+  Canvas.Brush.Color := Color;
   Canvas.RoundRect(0, 0, Width, Height, RoundValue, RoundValue);
+
+  Canvas.Pen.Color := Color;
+  Canvas.Rectangle(0,0,RoundValue,RoundValue);
+
+  If FBorder then begin
+    Canvas.Pen.Color := FBorderColor;
+    Canvas.MoveTo(0,0);
+    Canvas.LineTo(0,RoundValue);
+    Canvas.MoveTo(0,0);
+    Canvas.LineTo(RoundValue,0);
+  end;
 end;
 
 procedure TSharpERoundPanel.Resize;
 var
-  H: HRgn;
+  H, H2, H3: HRgn;
 begin
   Inherited;
   H := CreateRoundRectRgn(0, 0, Width + 1, Height + 1, FRoundValue, FRoundValue);
-  SetWindowRgn(Handle, H, True);
+  H2 := CreateRectRgn(0,0,FRoundValue,FRoundValue);
+  H3 := CombineRgn(H2,H2,H,RGN_AND);
+
+  SetWindowRgn(Handle, H3, True);
+  Invalidate; 
+end;
+
+procedure TSharpERoundPanel.SetBackgroundColor(const Value: TColor);
+begin
+  FBackgroundColor := Value;
+  Invalidate;
+end;
+
+procedure TSharpERoundPanel.SetBorder(const Value: Boolean);
+begin
+  FBorder := Value;
   Invalidate;
 end;
 
