58222154e436d8cb5d2f04e73d8095aee6ccfce0
Billi Berserker <billiberserker@sharpe.gov>
2007-03-14 15:25:54 +0000

    * moved Units and Forms to sub directories

--- Components/SharpSkin/SharpSkin.dpr
+++ Components/SharpSkin/SharpSkin.dpr
@@ -32,16 +32,16 @@ program SharpSkin;
 
 uses
   Forms,
-  MainForm in 'MainForm.pas' {Form1},
-  Defaults in 'Defaults.pas',
-  BarForm in 'BarForm.pas' {BarWnd},
-  AboutWnd in 'AboutWnd.pas' {AboutForm},
-  QuickHelpWnd in 'QuickHelpWnd.pas' {QuickHelpForm},
+  MainForm in 'Forms\MainForm.pas' {Form1},
+  Defaults in 'Units\Defaults.pas',
+  BarForm in 'Forms\BarForm.pas' {BarWnd},
+  AboutWnd in 'Forms\AboutWnd.pas' {AboutForm},
+  QuickHelpWnd in 'Forms\QuickHelpWnd.pas' {QuickHelpForm},
+  SchemeEditWnd in 'Forms\SchemeEditWnd.pas' {SchemeEditForm},
   SharpFX in '..\..\Common\Units\SharpFX\SharpFX.pas',
   SharpAPI in '..\..\Common\Libraries\SharpAPI\SharpAPI.pas',
   graphicsFX in '..\..\Common\Units\SharpFX\graphicsFX.pas',
   GR32_PNG in '..\..\Common\3rd party\GR32 Addons\GR32_PNG.pas',
-  SchemeEditWnd in 'SchemeEditWnd.pas' {SchemeEditForm},
   uSharpeColorBox in '..\..\Common\Delphi Components\SharpEColorBox\uSharpeColorBox.pas';
 
 {$R *.res}
