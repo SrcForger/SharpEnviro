f8b5a03f8948142ede18d557643edc707bddb3fe
Lee Pixol <pixol@sharpe.gov>
2007-07-07 19:34:43 +0000

    * Added Vista manifest

--- Components/SetShell/SetShell.dpr
+++ Components/SetShell/SetShell.dpr
@@ -31,6 +31,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 program SetShell;
 
 uses
+  VistaTheme,
   Forms,
   MainWnd in 'Forms\MainWnd.pas' {MainForm},
   uShellSwitcher in 'Units\uShellSwitcher.pas',
