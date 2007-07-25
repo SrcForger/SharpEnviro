e02cf5c1c049f77fe0a61bbb2559db8cb20a9e39
Lee Pixol <pixol@sharpe.gov>
2007-07-25 22:43:56 +0000

    * First Commit

---
+++ Common/Libraries/SharpCenterApi/SharpCenterAPI.dpr
@@ -0,0 +1,271 @@
+{
+Source Name: SharpApi.dpr
+Description: API commands for SharpE
+Copyright (C) Lee Green (Pixol) <pixol@sharpe-shell.org>
+              Malx (Malx@techie.com)
+              Martin Krämer <MartinKraemer@gmx.net>
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe
+
+Main SharpE Site
+http://www.Sharpe-Shell.org
+
+Recommended Environment
+ - Compiler : Delphi 2005 (Personal Edition)
+ - OS : Windows 2000 or higher
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+library SharpCenterAPI;
+
+{$WARN UNIT_PLATFORM OFF}
+{$WARN SYMBOL_PLATFORM OFF}
+
+uses
+  windows,
+  messages,
+  SysUtils,
+  shellAPI,
+  SharpApi;
+
+{$R *.RES}
+const
+  WM_SHARPCENTERMESSAGE = WM_APP + 660;
+  
+  SCM_SET_EDIT_STATE = 1;
+  SCM_SET_EDIT_CANCEL_STATE = 2;
+  SCM_SET_BUTTON_ENABLED = 3;
+  SCM_SET_BUTTON_DISABLED = 4;
+  SCM_SET_TAB_SELECTED = 5;
+  SCM_SET_SETTINGS_CHANGED = 6;
+  SCM_EVT_UPDATE_PREVIEW = 8;
+  SCM_EVT_UPDATE_SETTINGS = 9;
+
+  SCC_LOAD_SETTING = '_loadsetting';
+  SCC_CHANGE_FOLDER = '_changedir';
+  SCC_UNLOAD_DLL = '_unloaddll';
+  SCC_LOAD_DLL = '_loaddll';
+
+Type
+  TSCC_COMMAND_ENUM = (sccLoadSetting, sccChangeFolder, sccUnloadDll, sccLoadDll);
+  TSCB_BUTTON_ENUM = (scbMoveUp, scbMoveDown, scbImport, scbExport, scbClear,
+    scbDelete, scbHelp, scbAddTab, scbEditTab, scbDeleteTab, scbConfigure);
+  TSU_UPDATE_ENUM = (suSkin, suSkinFileChanged, suScheme, suTheme, suIconSet,
+    suBackground, suService, suDesktopIcon, suSharpDesk, suSharpMenu,
+      suSharpBar, suCursor, suWallpaper);
+
+var
+  wpara: wparam;
+  lpara: lparam;
+
+{EXPORTED FUNTIONS}
+
+function BroadcastCenterMessage(wpar: wparam; lpar: lparam): boolean;
+var
+  wnd: hWnd;
+  MutexHandle: THandle;
+begin
+  Result := False;
+  wpara := wpar;
+  lpara := lpar;
+
+  MuteXHandle := OpenMutex(MUTEX_ALL_ACCESS, False, 'SharpCenterMutexX');
+    if MuteXHandle <> 0 then
+    begin
+
+      //Find the window
+      wnd := FindWindow('TSharpCenterWnd', nil);
+      if wnd <> 0 then
+      begin
+        Result := True;
+        PostMessage(wnd, WM_SHARPCENTERMESSAGE, wpara, lpara);
+      end else
+        Result := False;
+      CloseHandle(MuteXHandle);
+    end
+end;
+
+function CenterCommand(ACommand: TSCC_COMMAND_ENUM; AParam, APluginID :PChar): hresult;
+var
+  cds: TCopyDataStruct;
+  wnd: hWnd;
+  msg: TSharpE_DataStruct;
+  path: string;
+  MutexHandle: THandle;
+
+  sCommand: String;
+begin
+  Result := 0;
+  wnd := 0;
+
+  Case ACommand of
+    sccLoadSetting: sCommand := SCC_LOAD_SETTING;
+    sccChangeFolder: sCommand := SCC_CHANGE_FOLDER;
+    sccUnloadDll: sCommand := SCC_UNLOAD_DLL;
+    sccLoadDll: sCommand := SCC_LOAD_DLL;
+    else
+      sCommand := '';
+  end;
+
+  // Check valid command
+  if sCommand = '' then begin
+    SendDebugMessageEx('SharpApi', pchar(format('Config Msg Invalid: %s',
+      [sCommand])), 0, DMT_INFO);
+    exit;
+  end;
+
+  try
+
+    SendDebugMessageEx('SharpApi', pchar(format('Config Msg Received: %s - %s',
+      [sCommand, AParam])), 0, DMT_INFO);
+    msg.Parameter := AParam;
+    msg.Command := sCommand;
+    msg.PluginID := APluginID;
+
+    with cds do
+    begin
+      dwData := 0;
+      cbData := SizeOf(TSharpE_DataStruct);
+      lpData := @msg;
+    end;
+
+    MuteXHandle := OpenMutex(MUTEX_ALL_ACCESS, False, 'SharpCenterMutexX');
+    if MuteXHandle <> 0 then
+    begin
+
+      //Find the window
+      wnd := FindWindow('TSharpCenterWnd', nil);
+      if wnd <> 0 then
+      begin
+        SendDebugMessageEx('SharpApi',
+          pchar(format('SharpCenter Mutex Exists, Sending Msg: %s - %s',
+            [sCommand, AParam])),
+          0,
+          DMT_STATUS);
+        result := sendmessage(wnd, WM_COPYDATA, 0, Cardinal(@cds));
+      end;
+      CloseHandle(MuteXHandle);
+    end
+    else
+    begin
+      // Start the SharpCenter application
+      Path := GetSharpeDirectory;
+
+      if fileexists(Path + 'SharpCenter.exe') then
+      begin
+        SendDebugMessageEx('SharpApi',
+          pchar(format('SharpCenter Mutex not found, Launching file: %s', [Path
+            +
+          'SharpCenter.exe'])), 0, DMT_STATUS);
+        ShellExecute(wnd, 'open', pchar(Path + 'SharpCenter.exe'), Pchar('-api '
+          +
+          sCommand + ' ' + AParam), pchar(path), WM_SHOWWINDOW);
+      end
+      else
+        SendDebugMessageEx('SharpApi',
+          pchar(format('There was an error launching: %s', [Path +
+          'SharpCenter.exe'])), 0, DMT_ERROR);
+    end;
+
+  except
+    result := HR_UNKNOWNERROR;
+  end;
+end;
+
+function BroadcastGlobalUpdateMessage(AUpdateType:TSU_UPDATE_ENUM;
+  APluginID: Integer=-1) : boolean;
+begin
+  Result := True;
+  SharpEBroadCast(WM_SHARPEUPDATESETTINGS, Integer(AUpdateType), APluginID);
+end;
+
+function CenterDefineEditState(AEditing: Boolean): boolean;
+begin
+  if AEditing then
+    Result := BroadcastCenterMessage(SCM_SET_EDIT_STATE,0) else
+    Result := BroadcastCenterMessage(SCM_SET_EDIT_CANCEL_STATE,0);
+end;
+
+function CenterDefineButtonState(AButton: TSCB_BUTTON_ENUM; AEnabled: Boolean): boolean;
+begin
+  if AEnabled then
+    Result := BroadcastCenterMessage(SCM_SET_BUTTON_ENABLED,Integer(AButton)) else
+    Result := BroadcastCenterMessage(SCM_SET_BUTTON_DISABLED,Integer(AButton));
+end;
+
+function CenterDefineSettingsChanged: boolean;
+begin
+  Result := BroadcastCenterMessage(SCM_SET_SETTINGS_CHANGED,0);
+end;
+
+function CenterSelectEditTab(AEditTab: TSCB_BUTTON_ENUM): boolean;
+begin
+  Result := False;
+  case AEditTab of
+    scbAddTab,scbEditTab,scbDeleteTab:
+      Result := BroadcastCenterMessage(SCM_SET_TAB_SELECTED, integer(AEditTab));
+  end;
+end;
+
+function CenterUpdatePreview: boolean;
+begin
+  Result := BroadcastCenterMessage(SCM_EVT_UPDATE_PREVIEW,0);
+end;
+
+function CenterUpdateSettings: boolean;
+begin
+  Result := BroadcastCenterMessage(SCM_EVT_UPDATE_SETTINGS,0);
+end;
+
+function CenterCommandAsText(ACommand: TSCC_COMMAND_ENUM): string;
+begin
+  if ACommand = sccLoadSetting then result := SCC_LOAD_SETTING else
+  if ACommand = sccChangeFolder then result := SCC_CHANGE_FOLDER else
+  if ACommand = sccUnloadDll then result := SCC_UNLOAD_DLL else
+  if ACommand = sccLoadDll then result := SCC_LOAD_DLL;
+end;
+
+function CenterCommandAsEnum(ACommand: string): TSCC_COMMAND_ENUM;
+begin
+  Result := sccLoadSetting;
+
+  if CompareText(ACommand,SCC_LOAD_SETTING) = 0 then
+    result := sccLoadSetting else
+  if CompareText(ACommand,SCC_CHANGE_FOLDER) = 0 then
+    result := sccChangeFolder else
+  if CompareText(ACommand,SCC_UNLOAD_DLL) = 0 then
+    result := sccUnloadDll else
+  if CompareText(ACommand,SCC_LOAD_DLL) = 0 then
+    result := sccLoadDll;
+end;
+
+exports
+  BroadcastGlobalUpdateMessage,
+  BroadcastCenterMessage,
+  CenterCommand,
+  CenterDefineEditState,
+  CenterDefineButtonState,
+  CenterDefineSettingsChanged,
+  CenterSelectEditTab,
+  CenterUpdatePreview,
+  CenterUpdateSettings,
+  CenterCommandAsText,
+  CenterCommandAsEnum;
+begin
+
+end.
+
---
+++ Common/Libraries/SharpCenterApi/SharpCenterAPI.pas
@@ -0,0 +1,103 @@
+{
+Source Name: SharpAPI
+Description: Header unir for SharpAPI.dll
+Copyright (C) Lee Green (Pixol) <pixol@sharpe-shell.org>
+              Malx (Malx@techie.com)
+              Martin Krämer <MartinKraemer@gmx.net>
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+Main SharpE Site
+http://www.Sharpe-Shell.org
+
+Recommended Environment
+ - Compiler : Delphi 2005 (Personal Edition)
+ - OS : Windows 2000 or higher
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+unit SharpCenterAPI;
+
+interface
+
+uses
+  Messages,
+  Windows;
+
+const
+  SCM_SET_EDIT_STATE = 1;
+  SCM_SET_EDIT_CANCEL_STATE = 2;
+  SCM_SET_BUTTON_ENABLED = 3;
+  SCM_SET_BUTTON_DISABLED = 4;
+  SCM_SET_TAB_SELECTED = 5;
+  SCM_SET_SETTINGS_CHANGED = 6;
+  SCM_EVT_UPDATE_PREVIEW = 8;
+  SCM_EVT_UPDATE_SETTINGS = 9;
+
+const
+  WM_SHARPCENTERMESSAGE = WM_APP + 660;
+
+Type
+  TSCC_COMMAND_ENUM = (sccLoadSetting, sccChangeFolder, sccUnloadDll, sccLoadDll);
+  TSCB_BUTTON_ENUM = (scbMoveUp, scbMoveDown, scbImport, scbExport, scbClear,
+    scbDelete, scbHelp, scbAddTab, scbEditTab, scbDeleteTab, scbConfigure);
+  TSU_UPDATE_ENUM = (suSkin, suSkinFileChanged, suScheme, suTheme, suIconSet,
+    suBackground, suService, suDesktopIcon, suSharpDesk, suSharpMenu,
+      suSharpBar, suCursor, suWallpaper);
+  TSCE_EDITMODE_ENUM = (sceAdd, sceEdit, sceDelete);
+
+function BroadcastGlobalUpdateMessage(AUpdateType:TSU_UPDATE_ENUM;
+  APluginID: Integer=-1) : boolean;
+    external 'SharpCenterAPI.dll' name 'BroadcastGlobalUpdateMessage';
+
+function BroadcastCenterMessage(wpar: wparam; lpar: lparam): boolean;
+  external 'SharpCenterAPI.dll' name 'BroadcastCenterMessage';
+
+function CenterCommand(ACommand: TSCC_COMMAND_ENUM; AParam, APluginID :PChar): hresult;
+  external 'SharpCenterAPI.dll' name 'CenterCommand';
+
+function CenterDefineEditState(AEditing: Boolean): boolean;
+  external 'SharpCenterAPI.dll' name 'CenterDefineEditState';
+
+function CenterDefineButtonState(AButton: TSCB_BUTTON_ENUM; AEnabled: Boolean): boolean;
+  external 'SharpCenterAPI.dll' name 'CenterDefineButtonState';
+
+function CenterDefineSettingsChanged: boolean;
+  external 'SharpCenterAPI.dll' name 'CenterDefineSettingsChanged';
+
+function CenterSelectEditTab(AEditTab: TSCB_BUTTON_ENUM): boolean;
+  external 'SharpCenterAPI.dll' name 'CenterSelectEditTab';
+
+function CenterUpdatePreview: boolean;
+  external 'SharpCenterAPI.dll' name 'CenterUpdatePreview';
+
+function CenterUpdateSettings: boolean;
+  external 'SharpCenterAPI.dll' name 'CenterUpdateSettings';
+
+function CenterCommandAsText(ACommand: TSCC_COMMAND_ENUM): string;
+  external 'SharpCenterAPI.dll' name 'CenterCommandAsText';
+
+function CenterCommandAsEnum(ACommand: string): TSCC_COMMAND_ENUM;
+  external 'SharpCenterAPI.dll' name 'CenterCommandAsEnum';
+
+implementation
+
+end.
+
+
+
+
