4c50c082f4a52754f52fcd63add871598c93f2fe
Billi Berserker <billiberserker@sharpe.gov>
2007-08-24 23:47:56 +0000

    * first commit of Font config

---
+++ Plugins/Configurations/Font/Font.dpr
@@ -0,0 +1,226 @@
+﻿{
+Source Name: Font
+Description: Font Config Dll
+Copyright (C) Martin Krämer (MartinKraemer@gmx.net)
+
+3rd Party Libraries used: JCL, JVCL
+Common: SharpApi
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+Main SharpE Site
+http://www.sharpe-shell.org
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+library Font;
+uses
+  Controls,
+  Classes,
+  Windows,
+  Forms,
+  Math,
+  Dialogs,
+  JvSimpleXml,
+  GR32,
+  GR32_Image,
+  PngSpeedButton,
+  uVistaFuncs,
+  SysUtils,
+  Graphics,
+  SharpEFontSelectorFontList,
+  uFontWnd in 'uFontWnd.pas' {frmFont},
+  SharpAPI in '..\..\..\Common\Libraries\SharpAPI\SharpAPI.pas',
+  uSEListboxPainter in '..\..\..\Common\Units\SEListboxPainter\uSEListboxPainter.pas',
+  SharpFX in '..\..\..\Common\Units\SharpFX\SharpFX.pas',
+  GR32_PNG in '..\..\..\Common\3rd party\GR32 Addons\GR32_PNG.pas',
+  graphicsFX in '..\..\..\Common\Units\SharpFX\graphicsFX.pas',
+  uSharpCenterPluginTabList in '..\..\..\Common\Units\SharpCenterSupporting\uSharpCenterPluginTabList.pas',
+  uSharpCenterCommon in '..\..\..\Common\Units\SharpCenterSupporting\uSharpCenterCommon.pas',
+  SharpIconUtils in '..\..\..\Common\Units\SharpIconUtils\SharpIconUtils.pas',
+  SharpCenterAPI in '..\..\..\Common\Libraries\SharpCenterApi\SharpCenterAPI.pas';
+
+{$E .dll}
+
+{$R *.res}
+
+procedure Save;
+begin
+  if frmFont = nil then
+    exit;
+
+  frmFont.SaveSettings;
+end;      
+
+function Open(const APluginID: Pchar; AOwner: hwnd): hwnd;
+var
+  XML : TJvSimpleXML;
+  FName : String;
+  n : integer;
+  s : string;
+begin
+  if frmFont = nil then frmFont := TfrmFont.Create(nil);
+
+  uVistaFuncs.SetVistaFonts(frmFont);
+  frmFont.sTheme := APluginID;
+  frmFont.ParentWindow := aowner;
+  frmFont.Left := 2;
+  frmFont.Top := 2;
+  frmFont.BorderStyle := bsNone;
+
+  FName := SharpApi.GetSharpeUserSettingsPath + '\Themes\'+APluginID+'\Font.xml';
+  if FileExists(FName) then
+  begin
+    XML := TJvSimpleXML.Create(nil);
+    try
+      XML.LoadFromFile(FName);
+      with frmFont do
+        with XML.Root.Items do
+        begin
+          if BoolValue('ModSize',False) then
+          begin
+            sgb_size.Value := IntValue('ValueSize',sgb_size.Value);
+            UIC_size.UpdateStatus;
+          end;
+
+          if BoolValue('ModName',False) then
+          begin
+            UIC_FontType.HasChanged := True;
+            s := Value('ValueName','');
+            for n  := 0 to FontList.List.Count - 1 do
+              if CompareText(TFontInfo(FontList.List.Objects[n]).FullName,s) = 0 then
+              begin
+                cbxFontName.ItemIndex := n;
+                break;
+              end;
+          end;
+
+          if BoolValue('ModAlpha',False) then
+          begin
+            sgb_Alpha.value := IntValue('ValueAlpha',sgb_Alpha.value);
+            UIC_Alpha.UpdateStatus;
+          end;
+
+          if BoolValue('ModUseShadow',False) then
+          begin
+            UIC_Shadow.HasChanged := True;
+            cb_shadow.Checked := BoolValue('ValueUseShadow',cb_shadow.Checked);
+          end;
+
+          if BoolValue('ModShadowType',False) then
+          begin
+            UIC_ShadowType.HasChanged := True;
+            cb_shadowtype.ItemIndex := Max(0,Min(3,IntValue('ValueShadowType',0)));
+          end;
+
+          if BoolValue('ModShadowAlpha',False) then
+          begin
+            sgb_shadowAlpha.Value := IntValue('ValueShadowAlpha',sgb_shadowAlpha.Value);
+            UIC_ShadowAlpha.UpdateStatus;            
+          end;
+
+          if BoolValue('ModBold',False) then
+          begin
+            UIC_Bold.HasChanged := True;
+            cb_bold.checked := BoolValue('ValueBold',cb_bold.checked);
+          end;
+
+          if BoolValue('ModItalic',False) then
+          begin
+            UIC_Italic.HasChanged := True;
+            cb_italic.checked := BoolValue('ValueItalic',cb_bold.checked);
+          end;
+
+          if BoolValue('ModUnderline',False) then
+          begin
+            UIC_Underline.HasChanged := True;
+            cb_underline.checked := BoolValue('ValueUnderline',cb_bold.checked);
+          end;
+        end;
+    except
+    end;
+    XML.Free;
+  end;
+
+  frmFont.Show;
+  result := frmFont.Handle;
+end;
+
+function Close : boolean;
+begin
+  result := True;
+  try
+    frmFont.Close;
+    frmFont.Free;
+    frmFont := nil;
+  except
+    result := False;
+  end;
+end;
+
+
+procedure SetDisplayText(const APluginID: Pchar; var ADisplayText: PChar);
+begin
+  ADisplayText := PChar('Font');
+end;
+
+procedure SetStatusText(var AStatusText: PChar);
+begin
+  AStatusText := '';
+end;
+
+procedure ClickBtn(AButtonID: Integer; AButton:TPngSpeedButton; AText:String);
+begin
+end;
+
+function SetBtnState(AButtonID: Integer): Boolean;
+begin
+  Result := False;
+end;
+
+procedure GetCenterScheme(var ABackground: TColor;
+      var AItemColor: TColor; var AItemSelectedColor: TColor);
+begin
+  if frmFont <> nil then
+  begin
+  end;
+end;
+
+procedure AddTabs(var ATabs:TPluginTabItemList);
+begin
+  ATabs.Add('Font',nil,'','');
+end;
+
+function SetSettingType: TSU_UPDATE_ENUM;
+begin
+  result := suSkin;
+end;
+
+
+exports
+  Open,
+  Close,
+  Save,
+  SetDisplayText,
+  SetStatusText,
+  SetBtnState,
+  GetCenterScheme,
+  AddTabs,
+  ClickBtn;
+
+end.
+
---
+++ Plugins/Configurations/Font/uFontWnd.pas
@@ -0,0 +1,286 @@
+﻿{
+Source Name: uFontWnd.pas
+Description: Font Window
+Copyright (C) Martin Krämer (MartinKraemer@gmx.net)
+
+3rd Party Libraries used: JCL, JVCL
+Common: SharpApi
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+Main SharpE Site
+http://www.sharpe-shell.org
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+unit uFontWnd;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, JvSimpleXml, uSEListboxPainter, JclFileUtils,
+  uSharpCenterPluginTabList, uSharpCenterCommon, ImgList, PngImageList,
+  SharpEListBox, SharpEListBoxEx, GR32, GR32_PNG, SharpApi,
+  ExtCtrls, Menus, JclStrings, GR32_Image, SharpEGaugeBoxEdit, SharpEUIC,
+  SharpEFontSelectorFontList;
+
+type
+  TStringObject = class(TObject)
+  public
+    Str: string;
+  end;
+
+type
+  TfrmFont = class(TForm)
+    Panel1: TPanel;
+    Label1: TLabel;
+    UIC_Size: TSharpEUIC;
+    sgb_size: TSharpeGaugeBox;
+    UIC_FontType: TSharpEUIC;
+    cbxFontName: TComboBox;
+    Label2: TLabel;
+    imlFontIcons: TImageList;
+    UIC_Alpha: TSharpEUIC;
+    sgb_alpha: TSharpeGaugeBox;
+    UIC_Shadow: TSharpEUIC;
+    cb_shadow: TCheckBox;
+    UIC_ShadowType: TSharpEUIC;
+    Label3: TLabel;
+    cb_shadowtype: TComboBox;
+    UIC_ShadowAlpha: TSharpEUIC;
+    sgb_shadowalpha: TSharpeGaugeBox;
+    UIC_Bold: TSharpEUIC;
+    cb_bold: TCheckBox;
+    UIC_Underline: TSharpEUIC;
+    cb_Underline: TCheckBox;
+    UIC_Italic: TSharpEUIC;
+    cb_Italic: TCheckBox;
+    procedure FormCreate(Sender: TObject);
+    procedure sgb_sizeChangeValue(Sender: TObject; Value: Integer);
+    procedure cbxFontNameDrawItem(Control: TWinControl; Index: Integer;
+      Rect: TRect; State: TOwnerDrawState);
+    procedure FormDestroy(Sender: TObject);
+    procedure cbxFontNameChange(Sender: TObject);
+    procedure sgb_alphaChangeValue(Sender: TObject; Value: Integer);
+    procedure cb_shadowClick(Sender: TObject);
+    procedure cb_shadowtypeChange(Sender: TObject);
+    procedure cb_boldClick(Sender: TObject);
+    procedure cb_ItalicClick(Sender: TObject);
+    procedure cb_UnderlineClick(Sender: TObject);
+    procedure sgb_shadowalphaChangeValue(Sender: TObject; Value: Integer);
+    procedure FormShow(Sender: TObject);
+  private
+    FFontList : TFontList;
+  public
+    sTheme: string;
+    procedure SaveSettings;
+    procedure RefreshFontList;
+    property FontList : TFontList read FFontList;
+  end;
+
+var
+  frmFont: TfrmFont;
+
+implementation
+
+uses SharpThemeApi, SharpCenterApi;
+
+{$R *.dfm}
+
+{ TfrmConfigListWnd }
+
+procedure TfrmFont.RefreshFontList;
+var
+  fi: TFontInfo;
+  i: integer;
+  DuplicateCheck: Integer;
+begin
+  FFontList.List.Clear;
+
+  cbxFontName.Items.Clear;
+  //cbxFontName.Sorted := True;
+  try
+    FFontList.RefreshFontInfo;
+    for i := 0 To pred(FFontList.List.Count) do begin
+      fi := TFontInfo(FFontList.List.Objects[i]);
+      DuplicateCheck := cbxFontName.Items.IndexOf(fi.FullName);
+
+      if DuplicateCheck = -1 then
+      cbxFontName.Items.Add(FFontList.List.Strings[i]);
+    end;
+  finally
+  end;
+end;
+
+procedure TfrmFont.SaveSettings;
+var
+  XML: TJvSimpleXML;
+  sDir: string;
+begin
+  sDir := SharpApi.GetSharpeUserSettingsPath + '\Themes\' + sTheme;
+
+  XML := TJvSimpleXML.Create(nil);
+  try
+    XML.Root.Name := 'ThemeFontSettings';
+    with XML.Root.Items do
+    begin
+      Add('ModSize',UIC_Size.HasChanged);
+      Add('ModName',UIC_FontType.HasChanged);
+      Add('ModAlpha',UIC_Alpha.HasChanged);
+      Add('ModUseShadow',UIC_Shadow.HasChanged);
+      Add('ModShadowType',UIC_ShadowType.HasChanged);
+      Add('ModShadowAlpha',UIC_ShadowAlpha.HasChanged);
+      Add('ModBold',UIC_Bold.HasChanged);
+      Add('ModItalic',UIC_Italic.HasChanged);
+      Add('ModUnderline',UIC_Underline.HasChanged);
+      Add('ValueSize',sgb_size.Value);
+      if cbxFontName.ItemIndex <> - 1 then
+        Add('ValueName',TFontInfo(FFontList.List.Objects[cbxFontName.ItemIndex]).FullName);
+      Add('ValueAlpha',sgb_Alpha.value);
+      Add('ValueUseShadow',cb_Shadow.checked);
+      Add('ValueShadowType',cb_shadowtype.ItemIndex);
+      Add('ValueShadowAlpha',sgb_shadowalpha.Value);
+      Add('ValueBold',cb_bold.checked);
+      Add('ValueItalic',cb_italic.checked);
+      Add('ValueUnderline',cb_underline.checked);
+    end;
+    XML.SaveToFile(sDir + '\Font.xml~');
+
+    if FileExists(sDir + '\Font.xml') then
+      DeleteFile(sDir + '\Font.xml');
+
+    RenameFile(sDir + '\Font.xml~', sDir + '\Font.xml');
+
+  finally
+    XML.Free;
+  end;
+end;
+
+procedure TfrmFont.sgb_alphaChangeValue(Sender: TObject; Value: Integer);
+begin
+  UIC_ALPHA.UpdateStatus;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.sgb_shadowalphaChangeValue(Sender: TObject; Value: Integer);
+begin
+  UIC_ShadowAlpha.UpdateStatus;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.sgb_sizeChangeValue(Sender: TObject; Value: Integer);
+begin
+  UIC_Size.UpdateStatus;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.cbxFontNameChange(Sender: TObject);
+begin
+  UIC_FontType.UpdateStatus;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.cbxFontNameDrawItem(Control: TWinControl; Index: Integer;
+  Rect: TRect; State: TOwnerDrawState);
+var
+  fi:TFontInfo;
+  imageindex : integer;
+  itemindex  : integer;
+  textheight : integer;
+begin
+  cbxFontName.canvas.fillrect(rect);
+
+  itemindex := FFontList.List.IndexOf(cbxFontName.Items.Strings[index]);
+  fi := TFontInfo(FFontList.List.Objects[itemindex]);
+
+  imageindex := -1;
+  case fi.FontType of
+    ftTrueType : imageindex := 0;
+    ftRaster   : imageindex := 1;
+    ftDevice   : imageindex := 2;
+  end;
+
+  imlFontIcons.Draw(cbxFontName.Canvas,rect.left,rect.top,imageindex);
+
+  cbxFontName.Canvas.Font.Name := fi.ShortName;
+  textheight := cbxFontName.Canvas.TextHeight(fi.FullName);
+
+  if textheight > cbxFontName.ItemHeight then
+     cbxFontName.Canvas.Font.Name := 'Arial';
+
+  cbxFontName.canvas.textout(rect.left+imlFontIcons.width+2,rect.top,fi.FullName);
+end;
+
+procedure TfrmFont.cb_boldClick(Sender: TObject);
+begin
+  UIC_Bold.UpdateStatus;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.cb_ItalicClick(Sender: TObject);
+begin
+  UIC_Italic.UpdateStatus;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.cb_shadowClick(Sender: TObject);
+begin
+  UIC_Shadow.UpdateStatus;
+  if cb_shadow.Checked then
+  begin
+    UIC_ShadowType.Height := 31;
+    UIC_ShadowAlpha.Height := 29;
+  end else
+  begin
+    UIC_ShadowType.Height := 0;
+    UIC_ShadowAlpha.Height := 0;
+  end;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.cb_shadowtypeChange(Sender: TObject);
+begin
+  UIC_ShadowType.UpdateStatus;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.cb_UnderlineClick(Sender: TObject);
+begin
+  UIC_Underline.UpdateStatus;
+  SharpCenterApi.CenterDefineSettingsChanged;
+end;
+
+procedure TfrmFont.FormCreate(Sender: TObject);
+begin
+  DoubleBuffered := true;
+  FFontList := TFontList.Create;
+  RefreshFontList;
+end;
+
+procedure TfrmFont.FormDestroy(Sender: TObject);
+begin
+  FFontList.Free;
+end;
+
+procedure TfrmFont.FormShow(Sender: TObject);
+begin
+  cb_shadow.OnClick(cb_shadow);
+end;
+
+end.
+
