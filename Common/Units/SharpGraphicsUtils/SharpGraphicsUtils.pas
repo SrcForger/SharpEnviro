9c1df5b86c0c31c3ac1e226c14b3c81c95420c39
Billi Berserker <billiberserker@sharpe.gov>
2007-09-19 12:01:06 +0000

    * removed HSLtoRGB and RGBtoHSL functions (they didn't work properly) * replaced usage of HSLtoRGB and RGBtoHSL with GR32 functions

--- Common/Units/SharpGraphicsUtils/SharpGraphicsUtils.pas
+++ Common/Units/SharpGraphicsUtils/SharpGraphicsUtils.pas
@@ -45,8 +45,6 @@ type
   procedure ReplaceTransparentAreas(Dst,AlphaSource : TBitmap32; ReplaceColor : TColor32);
 
   procedure HSLChangeImage(bmp : Tbitmap32; HMod,SMod,LMod : integer);
-  function HSLtoRGB(H,S,L : integer): TColor32;
-  function RGBtoHSL(RGB: TColor32) : THslColor;
 
   procedure VGradient(Bmp : TBitmap32; color1,color2 : TColor; st,et : byte; Rect : TRect);
   procedure HGradient(Bmp : TBitmap32; color1,color2 : TColor; st,et : byte; Rect : TRect);
@@ -238,84 +236,14 @@ begin
   end;
 end;
 
-function HSLtoRGB(H,S,L : integer): TColor32;
-var
-  V, M, M1, M2, VSF: Integer;
-begin
-  Result := Color32(0,0,0,0);
-  if L <= $7F then
-    V := L * (256 + S) shr 8
-  else
-    V := L + S - L * S div 255;
-  if V > 0 then begin
-    M := L * 2 - V;
-    H := H * 6;
-    VSF := (V - M) * (H and $ff) shr 8;
-    M1 := M + VSF;
-    M2 := V - VSF;
-    case H shr 8 of
-      0: Result := Color32(V, M1, M, 0);
-      1: Result := Color32(M2, V, M, 0);
-      2: Result := Color32(M, V, M1, 0);
-      3: Result := Color32(M, M2, V, 0);
-      4: Result := Color32(M1, M, V, 0);
-      5: Result := Color32(V, M, M2, 0);
-    end;
-  end;
-end;
-
-function RGBtoHSL(RGB: TColor32) : THslColor;
-var
-  R, G, B, D, Cmax, Cmin, HL: Integer;
-  H,S,L : integer;
-begin
-  R := (RGB shr 16) and $ff;
-  G := (RGB shr 8) and $ff;
-  B := RGB and $ff;
-  Cmax := Max2(R, G, B);
-  Cmin := Min2(R, G, B);
-  L := (Cmax + Cmin) div 2;
-
-  if Cmax = Cmin then
-  begin
-    H := 0;
-    S := 0
-  end
-  else
-  begin
-    D := (Cmax - Cmin) * 255;
-    if L <= $7F then
-      S := D div (Cmax + Cmin)
-    else
-      S := D div (255 * 2 - Cmax - Cmin);
-
-    D := D * 6;
-    if R = Cmax then
-      HL := (G - B) * 255 * 255 div D
-    else if G = Cmax then
-      HL := 255 * 2 div 6 + (B - R) * 255 * 255 div D
-    else
-      HL := 255 * 4 div 6 + (R - G) * 255 * 255 div D;
-
-    if HL < 0 then HL := HL + 255 * 2;
-    H := HL;
-  end;
-  with result do begin
-    hue := h;
-    saturation := s;
-    Lightness := l;
-  end;
-end;
-
 procedure HSLChangeImage(bmp : Tbitmap32; HMod,SMod,LMod : integer);
 var
   P          : PColor32;
   i          : integer;
   alpha      : byte;
   new_hsl    : THslColor;
-  hsl        : THSLColor;
-
   new_rgb    : TColor32;
+  h,s,l      : byte; 
 begin
   with bmp do
   try
@@ -325,10 +253,10 @@ begin
       alpha := (P^ shr 24);
       if alpha > 0 then
       begin
-        hsl := RGBToHSL(P^);
-        new_hsl.Hue := hsl.Hue + HMod;
-        new_hsl.Saturation := hsl.Saturation + SMod;
-        new_hsl.Lightness := hsl.Lightness + LMod;
+        RGBToHSL(WinColor(P^),h,s,l);
+        new_hsl.Hue := h + HMod;
+        new_hsl.Saturation := s + SMod;
+        new_hsl.Lightness := l + LMod;
 
         new_hsl.Hue := Min(Max(new_hsl.Hue,0),255);
         new_hsl.Saturation := Min(Max(new_hsl.Saturation,0),255);
@@ -366,8 +294,16 @@ var
   average_b    : Integer;
 
   tempbmp     : TBitmap32;
+
+  h,s,l : byte;
 begin
-  source_hsl := RGBToHSL(color32(color));
+  RGBToHSL(RGB((color and $00FF0000) shr 16,
+               (color and $0000FF00) shr 8,
+                color and $000000FF),
+                h,s,l);
+  source_hsl.Hue := h;
+  source_hsl.Lightness := l;
+  source_hsl.Saturation := s;
 
   if (blendalpha < 1) then exit;
 
@@ -394,7 +330,13 @@ begin
       for I := 0 to Width * Height - 1 do begin
          alpha := (P^ shr 24);
          if alpha <> 0 then begin
-           hsl[i] := RGBToHSL(P^);
+           RGBToHSL(RGB((P^ and $00FF0000) shr 16,
+                        (P^ and $0000FF00) shr 8,
+                         P^ and $000000FF),
+                         h,s,l);
+           hsl[i].Hue := h;
+           hsl[i].Lightness := l;
+           hsl[i].Saturation := s;
            inc(weighted_sum,hsl[i].hue*hsl[i].saturation);
            inc(total_weight,hsl[i].saturation);
            inc(sum_b, hsl[i].Lightness);
