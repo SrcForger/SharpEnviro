0996ea9f1d06a5a6116e94de1b157f10a779f1a1
Billi Berserker <billiberserker@sharpe.gov>
2007-09-21 14:27:03 +0000

    * removed broken BoxBlur function and replaced it with a new fast blur function

--- Common/Units/SharpGraphicsUtils/SharpGraphicsUtils.pas
+++ Common/Units/SharpGraphicsUtils/SharpGraphicsUtils.pas
@@ -37,7 +37,7 @@ type
    Lightness : Integer;
   end;
 
-  procedure boxblur(img: tbitmap32; radius: integer; iterations: integer; horz: boolean = true; vert: boolean = true);
+  procedure FastBlur(aBitmap32: TBitmap32; aRadius: Integer; aPasses: Integer = 2);
   procedure lightenBitmap(bmp : Tbitmap32; amount :integer); overload;
   procedure LightenBitmap(Bmp : TBitmap32; Amount :integer; Rect : TRect); overload;
 
@@ -492,144 +492,139 @@ begin
   result := Color32(R,G,B,A);
 end;
 
-procedure boxblur(img: tbitmap32; radius: integer; iterations: integer; horz: boolean = true; vert: boolean = true);
+// source: Graphics32 Newsgroup
+procedure FastBlur(aBitmap32: TBitmap32; aRadius: Integer; aPasses: Integer = 2);
+// Quick box blur algoritm
+
+// aPasses:
+// 1: Blur quality too low
+// 2: Best speed / quality compromise
+// 3: Good quality but impossible to have a small blur radius. Even radius 1 gives a large blur.
+
+type
+   PSumRecord = ^TSumRecord;
+   TSumRecord = packed record
+     B, G, R, A: cardinal;
+   end;
+
 var
-  gSum: cardinal; bSum: cardinal; rSum: cardinal; aSum: Cardinal;
-  line: PColor32Array;
-  col: tcolor32;
-  winlength: longword;
-  window, cx, x, y, t1, iter: integer;
-  realwidth, width, height: integer;
-  scanjump: integer;
-  rightedge, leftedge, center: PColor32;
-  pixelbuf: array of TColor32;
+  iPass:        integer;
+  lBoxSize:     cardinal;
+  lColor32:     TColor32;
+  lHeight1:     integer;
+  lSumArray:    array of TSumRecord;
+  lWidth1:      integer;
+  x:            integer;
+  xBitmap:      integer;
+  y:            integer;
+  yBitmap:      integer;
 begin
-  realwidth := img.width;
-  width := img.width - 1;
-  height := img.height - 1;
-  winlength := radius * 2 + 1;
-  scanjump := integer(img.scanline[1]) - integer(img.scanline[0]);
-
-  if horz then begin
-    setlength(pixelbuf, img.width);
-    for iter := 0 to iterations - 1 do begin
-      line := img.scanline[0];
-
-      for y := 0 to height do begin
-        rsum := 0;
-        gsum := 0;
-        bsum := 0;
-        aSum := 0;
-    //load up our initial window
-        for window := -radius to radius - 1 do begin //-1 so we don't include the first pixel to enter the window
-          //"Wrap" our edge pixel
-          if window < 0 then
-            cx := 0 else
-            cx := window;
-
-          rSum := rSum + (line[cx]) and $FF;
-          gSum := gSum + (line[cx] shr 8) and $FF;
-          bSum := bSum + (line[cx] shr 16) and $FF;
-          aSum := aSum + (line[cx] shr 24);
-        end;
-
-        leftedge := @line[0];
-        rightedge := @line[radius]; //start loading pixels in the end of the window
-        center := @pixelbuf[0];
-
-        for x := 0 to width do begin
-          col := rightedge^; //add the pixel at the right edge of the window
-          rSum := rSum + col and $FF;
-          gSum := gSum + (col shr 8) and $FF;
-          bSum := bSum + (col shr 16) and $FF;
-          aSum := aSum + (col shr 24);
-
-          center^ := (bSum div winlength) shl 16 or (gSum div winlength) shl 8 or (rSum div winlength) or (aSum div winlength) shl 24;
-
-      //unload the leftmost pixel
-          col := leftedge^;
-          rSum := rSum - (col and $FF);
-          gSum := gSum - ((col shr 8) and $FF);
-          bSum := bSum - ((col shr 16) and $FF);
-          aSum := aSum - (col shr 24);
-
-          if x < width - radius then
-            inc(rightedge);
-          if x >= radius then
-            inc(leftedge);
-          inc(center);
-        end;
-
-        Move(pixelbuf[0], line[0], 4 * (width + 1)); //copy the line we built to the bmp
-        line := Pointer(integer(line) + scanjump); //move to next line
-      end;
-    end; //horz iterations
+  if aRadius <= 0 then
+  begin
+   Exit;
   end;
 
+  lBoxSize := (aRadius * 2) + 1;
 
-  if vert then begin
-    setlength(pixelbuf, img.height);
-    for iter := 0 to iterations - 1 do begin
-      line := img.scanline[0];
+  lWidth1  := aBitmap32.Width  - 1;
+  lHeight1 := aBitmap32.Height - 1;
 
-      for x := 0 to width do begin
-        rsum := 0;
-        gsum := 0;
-        bsum := 0;
-        asum := 0;
+  // Process horizontally
+  SetLength(lSumArray, aBitmap32.Width + 2 * aRadius + 1);
 
-    //load up our initial window
-        for window := -radius to radius - 1 do begin //-1 so we don't include the first pixel to enter the window
-          //"Wrap" our edge pixel
-          if window < 0 then
-            cx := 0 else
-            cx := window;
+  for yBitmap := 0 to lHeight1 do
+  begin
 
-          rSum := rSum + (line[cx * realwidth]) and $FF;
-          gSum := gSum + (line[cx * realwidth] shr 8) and $FF;
-          bSum := bSum + (line[cx * realwidth] shr 16) and $FF;
-          aSum := aSum + (line[cx * realwidth] shr 24);
+    for iPass := 1 to aPasses do
+    begin
+
+      // First element is zero
+      lSumArray[0].A := 0;
+      lSumArray[0].R := 0;
+      lSumArray[0].G := 0;
+      lSumArray[0].B := 0;
+
+      for x := Low(lSumArray) + 1 to High(lSumArray) do
+      begin
+        xBitmap := x - aRadius - 1;
+
+        if xBitmap < 0 then
+        begin
+          xBitmap := 0;
+        end
+        else if xBitmap > lWidth1 then
+        begin
+          xBitmap := lWidth1;
         end;
 
-        leftedge := @line[0];
-        rightedge := @line[radius * realwidth]; //start loading pixels in the end of the window
-        center := @pixelbuf[0];
+        lColor32 := PColor32(aBitmap32.PixelPtr[xBitmap, yBitmap])^;
 
-        for y := 0 to height do begin
-          col := rightedge^; //add the pixel at the right edge of the window
-          rSum := rSum + col and $FF;
-          gSum := gSum + (col shr 8) and $FF;
-          bSum := bSum + (col shr 16) and $FF;
-          aSum := aSum + (col shr 24);
+        lSumArray[x].A := lSumArray[x - 1].A + lColor32 shr 24;
+        lSumArray[x].R := lSumArray[x - 1].R + lColor32 shr 16 and $FF;
+        lSumArray[x].G := lSumArray[x - 1].G + lColor32 shr 8  and $FF;
+        lSumArray[x].B := lSumArray[x - 1].B + lColor32        and $FF;
+      end;
 
-          center^ := (bSum div winlength) shl 16 or (gSum div winlength) shl 8 or (rSum div winlength) or (asum div winlength) shl 24;
+      for xBitmap := 0 to lWidth1 do
+      begin
+        x := xBitmap + aRadius + 1;
 
-      //unload the leftmost pixel
-          col := leftedge^;
-          rSum := rSum - (col and $FF);
-          gSum := gSum - ((col shr 8) and $FF);
-          bSum := bSum - ((col shr 16) and $FF);
-          aSum := aSum - (col shr 24);
+        PColor32(aBitmap32.PixelPtr[xBitmap, yBitmap])^ :=
+          ((lSumArray[x + aRadius].A - lSumArray[x - aRadius - 1].A) div lBoxSize) shl 24 or
+          ((lSumArray[x + aRadius].R - lSumArray[x - aRadius - 1].R) div lBoxSize) shl 16 or
+          ((lSumArray[x + aRadius].G - lSumArray[x - aRadius - 1].G) div lBoxSize) shl 8 or
+           (lSumArray[x + aRadius].B - lSumArray[x - aRadius - 1].B) div lBoxSize;
+      end;
+    end;
+  end;
 
-          if y < height - radius then
-            inc(rightedge, realwidth);
-          if y >= radius then
-            inc(leftedge, realwidth);
-          inc(center);
+  // Process vertically
+  SetLength(lSumArray, aBitmap32.Height + 2 * aRadius + 1);
+
+  for xBitmap := 0 to lWidth1 do
+  begin
+    for iPass := 1 to aPasses do
+    begin
+      // First element is zero
+      lSumArray[0].A := 0;
+      lSumArray[0].R := 0;
+      lSumArray[0].G := 0;
+      lSumArray[0].B := 0;
+
+      for y := Low(lSumArray) + 1 to High(lSumArray) do
+      begin
+        yBitmap := y - aRadius - 1;
+
+        if yBitmap < 0 then
+        begin
+          yBitmap := 0;
+        end
+        else if yBitmap > lHeight1 then
+        begin
+          yBitmap := lHeight1;
         end;
 
-        leftedge := @pixelbuf[0]; // Input
-        rightedge := @line[0]; // Output
-        for t1 := 0 to height do begin
-          rightedge^ := leftedge^;
-          inc(rightedge, realwidth);
-          inc(leftedge);
-        end;
+        lColor32 := PColor32(aBitmap32.PixelPtr[xBitmap, yBitmap])^;
 
-        inc(line); //move to next col
+        lSumArray[y].A := lSumArray[y - 1].A + lColor32 shr 24;
+        lSumArray[y].R := lSumArray[y - 1].R + lColor32 shr 16 and $FF;
+        lSumArray[y].G := lSumArray[y - 1].G + lColor32 shr 8  and $FF;
+        lSumArray[y].B := lSumArray[y - 1].B + lColor32        and $FF;
+      end;
+
+      for yBitmap := 0 to lHeight1 do
+      begin
+        y := yBitmap + aRadius + 1;
+
+        PColor32(aBitmap32.PixelPtr[xBitmap, yBitmap])^ :=
+          ((lSumArray[y + aRadius].A - lSumArray[y - aRadius - 1].A) div lBoxSize) shl 24 or
+          ((lSumArray[y + aRadius].R - lSumArray[y - aRadius - 1].R) div lBoxSize) shl 16 or
+          ((lSumArray[y + aRadius].G - lSumArray[y - aRadius - 1].G) div lBoxSize) shl 8  or
+           (lSumArray[y + aRadius].B - lSumArray[y - aRadius - 1].B) div lBoxSize;
       end;
     end;
   end;
 end;
+
 
 end.
