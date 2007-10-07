f420b9e4a3e24859965c2fd4123201f09d28ba92
Viking 1304 <viking1304@sharpe.gov>
2007-10-07 21:01:15 +0000

    TSharpESkinManager Create methods fix

--- Common/Delphi Components/SharpE Skin Components/SharpESkinManager.pas
+++ Common/Delphi Components/SharpE Skin Components/SharpESkinManager.pas
@@ -83,8 +83,8 @@ type
       override;
     procedure Loaded; override;
   public
-    constructor Create(AOwner: TComponent); overload; override;
-    constructor Create(AOwner: TComponent; Skins : TSharpESkinItems = ALL_SHARPE_SKINS); overload;
+    constructor Create(AOwner: TComponent); reintroduce; overload;
+    constructor Create(AOwner: TComponent; Skins : TSharpESkinItems = ALL_SHARPE_SKINS); reintroduce; overload;
     constructor CreateRuntime(AOwner: TComponent; Skin : TSharpESkin; Scheme : TSharpEScheme; Skins : TSharpESkinItems = ALL_SHARPE_SKINS); overload;
     constructor CreateRuntime(AOwner: TComponent; Skin : TSharpESkin; Scheme : TSharpEScheme; NoSystemScheme : boolean; Skins : TSharpESkinItems = ALL_SHARPE_SKINS); overload;
     destructor Destroy; override;
