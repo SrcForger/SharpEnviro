336f88445097cbd96d63dffc091aa5823d23e2b2
Sharpe MC <sharpe_mc@sharpe.gov>
2007-11-03 21:24:07 +0000

    * Switched from GetMetaData function to string table in metadata.res

--- Components/SharpCompile/SharpCompile.dpr
+++ Components/SharpCompile/SharpCompile.dpr
@@ -1,11 +1,5 @@
 program SharpCompile;
 
-
-
-
-
-
-
 uses
   Forms,
   SharpApi,
@@ -15,21 +9,7 @@ uses
   DebugDialog in '..\..\Common\Units\DebugDialog\DebugDialog.pas';
 
 {$R *.res}
-function GetMetaData(): TMetaData;
-begin
-  with result do
-  begin
-    Name := 'SharpCompile';
-    Description := 'The fanciest build tool in creation';
-    Author := 'Nathan LaFreniere (mc@sharpe-shell.org)';
-    Version := '0.7.4.0';
-    DataType := tteComponent;
-    ExtraData := 'priority: 0| delay: 0';
-  end;
-end;
-
-exports
-  GetMetaData;
+{$R metadata.res}
 
 begin
   Application.Initialize;
