84b9abeba6bcd3771d093f7175adbc948bd12aa6
Lee Pixol <pixol@sharpe.gov>
2007-12-23 02:31:00 +0000

    * First Commit

---
+++ Plugins/Configurations/Home/Home.dpr
@@ -0,0 +1,98 @@
+﻿{
+Source Name: DeskArea
+Description: DeskArea Configuration
+Copyright (C) Martin Krämer (MartinKraemer@gmx.net)
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+SharpE Site
+http://www.sharpenviro.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+library Home;
+uses
+  Controls,
+  Classes,
+  Windows,
+  Forms,
+  Dialogs,
+  uVistaFuncs,
+  SysUtils,
+  Graphics,
+  uHomeWnd in 'uHomeWnd.pas' {frmHome},
+  SharpAPI in '..\..\..\Common\Libraries\SharpAPI\SharpAPI.pas';
+
+{$E .dll}
+
+{$R *.res}
+
+function GetControlByHandle(AHandle: THandle): TWinControl;
+  begin
+    Try
+      Result := Pointer(GetProp(AHandle,
+      PChar(Format('Delphi%8.8x', [GetCurrentProcessID]))));
+    Except
+      Result := nil;
+    End;
+  end;
+
+function Open(const APluginID: Pchar; AOwner: hwnd): hwnd;
+begin
+  if frmHome = nil then frmHome := TfrmHome.Create(nil);
+  uVistaFuncs.SetVistaFonts(frmHome);
+
+  frmHome.ParentWindow := aowner;
+  frmHome.Left := 0;
+  frmHome.Top := 0;
+
+  frmHome.Show;
+  result := frmHome.Handle;
+end;
+
+function Close : boolean;
+begin
+  result := True;
+  try
+    frmHome.Close;
+    frmHome.Free;
+    frmHome := nil;
+  except
+    result := False;
+  end;
+end;
+
+function SetSettingType : TSU_UPDATE_ENUM;
+begin
+  result := suCenter;
+end;
+
+procedure SetText(const APluginID: String; var AName: String; var AStatus: String;
+  var ATitle: String; var ADescription: String);
+
+begin
+  ATitle := 'SharpCenter TD4';
+  ADescription := 'Welcome to SharpCenter. The ultimate configurator for SharpE.';
+end;
+
+exports
+  Open,
+  Close,
+  SetText,
+  SetSettingType;
+
+end.
+
---
+++ Plugins/Configurations/Home/uHomeWnd.pas
@@ -0,0 +1,174 @@
+﻿{
+Source Name: uDeskAreaSettingsWnd.pas
+Description: DeskArea Settings Window
+Copyright (C) Martin Krämer (MartinKraemer@gmx.net)
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+SharpE Site
+http://www.sharpenviro.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+unit uHomeWnd;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, pngimage, ExtCtrls, SharpCenterApi, StdCtrls, SharpEListBoxEx,
+  JvExControls, JvLabel, ImgList, PngImageList;
+
+type
+  TUser = Class
+    Name: String;
+    Handle: String;
+    Email: String;
+    Role: String;
+    constructor Create(AName, AHandle, AEmail, ARole: String);
+
+  End;
+
+  TfrmHome = class(TForm)
+    lblSharpETeam: TLabel;
+    lbUsers: TSharpEListBoxEx;
+    PngImageList1: TPngImageList;
+    lbWebsiteLinks: TSharpEListBoxEx;
+    Label1: TLabel;
+    imgLogo: TImage;
+    Shape1: TShape;
+    Shape2: TShape;
+    procedure FormCreate(Sender: TObject);
+    procedure lbUsersResize(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure lbUsersGetCellText(Sender: TObject; const ACol: Integer;
+      AItem: TSharpEListItem; var AColText: string);
+    procedure Label1Click(Sender: TObject);
+  private
+    FUsers: TList;
+    procedure AddUsersToList;
+    procedure AddItems;
+  public
+  end;
+
+var
+  frmHome: TfrmHome;
+
+const
+  colName = 0;
+  colEmail = 1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmHome.FormCreate(Sender: TObject);
+begin
+  FUsers := TList.Create;
+
+  AddUsersToList;
+  AddItems;
+
+  lbWebsiteLinks.AddItem('Trac - <u>http://trac.sharpe-shell.org',1);
+  lbWebsiteLinks.AddItem('Website - <u>http://www.sharpe-shell.org',1);
+  lbWebsiteLinks.AddItem('Sourceforge - <u>http://sourceforge.net/projects/sharpe/',1);
+End;
+
+procedure TfrmHome.FormDestroy(Sender: TObject);
+begin
+  FUsers.Free;
+end;
+
+procedure TfrmHome.Label1Click(Sender: TObject);
+begin
+
+end;
+
+{ TUser }
+
+
+procedure TfrmHome.AddItems;
+var
+  i: Integer;
+  tmpLi: TSharpEListItem;
+  tmpUser: TUser;
+begin
+  lbUsers.Clear;
+  for i := 0 to Pred(FUsers.Count) do begin
+    tmpUser := TUser(FUsers[i]);
+    tmpLi := lbUsers.AddItem(tmpUser.Name,0);
+    tmpLi.Data := tmpUser;
+
+  end;
+
+end;
+
+procedure TfrmHome.AddUsersToList;
+var
+  tmp: TUser;
+begin
+  tmp := TUser.Create('CoCo', 'Silentpyjamas', 'coco@sharpenviro.com', 'PR, Community and Pom Pom Queen.');
+  FUsers.Add(tmp);
+  tmp := TUser.Create('Florian', 'Captain Herisson', 'florian@sharpenviro.com', 'Graphics. Also the new Addons site developer.');
+  FUsers.Add(tmp);
+  tmp := TUser.Create('Glacialfury', 'Glacialfury', 'nathan@sharpenviro.com', 'Tester, and critical bug reporter.');
+  FUsers.Add(tmp);
+  tmp := TUser.Create('Lee', 'Pixol', 'lee@sharpenviro.com', 'Lead Developer of SharpCenter + Configurations.');
+  FUsers.Add(tmp);
+  tmp := TUser.Create('Martin', 'Billiberserker', 'martin@sharpenviro.com', 'Lead Developer of SharpBar, SharpMenu, Modules and well, too much.');
+  FUsers.Add(tmp);
+  tmp := TUser.Create('Nathan', 'Mc', 'nathan@sharpenviro.com', 'Lead Developer of SharpCore, and wonders like SharpCompile.');
+  FUsers.Add(tmp);
+  tmp := TUser.Create('Skizo', 'Skizo', 'skizo@sharpenviro.com', 'Graphics, and new guy.');
+  FUsers.Add(tmp);
+
+end;
+
+procedure TfrmHome.lbUsersGetCellText(Sender: TObject; const ACol: Integer;
+  AItem: TSharpEListItem; var AColText: string);
+var
+  tmp: TUser;
+begin
+  tmp := TUser(AItem.Data);
+  if tmp = nil then exit;
+
+  case ACol of
+    colName: begin
+      AColText := tmp.Name + ' (' + tmp.Handle + ')' + ' - <font color="clGray">' + tmp.Role;
+    end;
+    colEmail : AColText := '<u><font color="clNavy">Email</u>';
+  end;
+
+end;
+
+procedure TfrmHome.lbUsersResize(Sender: TObject);
+begin
+  Self.Height := imgLogo.Height + lbUsers.Height + lbWebsiteLinks.Height +
+    (lblSharpETeam.Height*2) + 30;
+end;
+
+{ TUser }
+
+constructor TUser.Create(AName, AHandle, AEmail, ARole: String);
+begin
+  Name := AName;
+  Handle := AHandle;
+  Email := AEmail;
+  Role := ARole;
+end;
+
+end.
+
