c7bb73ab541c97e98786d174cb182fecec8823fa
Lee Pixol <pixol@sharpe.gov>
2008-03-16 20:15:57 +0000

    * Units to manage task filters and SW Command enumerations

---
+++ Common/Units/TaskFilterList/SWCmdList.pas
@@ -0,0 +1,142 @@
+unit SWCmdList;
+
+interface
+
+uses
+  Windows,
+  Messages,
+  SysUtils,
+  Variants,
+  Classes,
+  Graphics,
+  Controls,
+  Contnrs,
+  Forms,
+  Dialogs,
+  ExtCtrls,
+  StdCtrls;
+
+type
+  TSWCmdEnum = (wscSW_Hide, wscSW_ShowNormal, wscSW_ShowMinimized,
+    wscSW_ShowMaximized, wscSW_ShowNoActivate, wscSW_Show, wscSW_Minimize,
+      wscSW_ShowMinNoActive, wscSW_ShowNA, wscSW_Restore, wscSW_ShowDefault,
+        wscSW_ForceMinimize);
+  TSWCmdEnums = set of TSWCmdEnum;
+
+
+  TSWCmdItem = Class(TPersistent)
+  private
+    FText: String;
+    FEnabled: Boolean;
+    FSWCmd: TSWCmdEnum;
+    function GetText: String;
+    function GetXmlText: string;
+  published
+    property Enabled: Boolean read FEnabled write FEnabled;
+    Property Text: String read GetText;
+    Property XmlText: string read GetXmlText;
+    Property SWCmd: TSWCmdEnum read FSWCmd write FSWCmd;
+    constructor Create(ASWCmd: TSWCmdEnum);
+  End;
+
+  TWindowShowCommandList = Class(TObjectList)
+  private
+    function GetItem(Index: Integer): TSWCmdItem;
+    procedure SetItem(Index: Integer; const Value: TSWCmdItem);
+  published
+  public
+    procedure AddItems;
+    function EnumCount: Integer;
+    property Item[Index: Integer] : TSWCmdItem
+             read GetItem write SetItem; Default;
+  End;
+
+implementation
+
+{ TWindowShowCommandItem }
+
+constructor TSWCmdItem.Create(
+  ASWCmd: TSWCmdEnum);
+begin
+  FSWCmd := ASWCmd;
+  FEnabled := False;
+end;
+
+function TSWCmdItem.GetText: String;
+begin
+  case FSWCmd of
+    wscSW_Hide: Result := 'SW_HIDE';
+    wscSW_ShowNormal: Result := 'SW_SHOWNORMAL, SW_NORMAL';
+    wscSW_ShowMinimized: Result := 'SW_SHOWMINIMIZED';
+    wscSW_ShowMaximized: Result := 'SW_MAXIMIZED, SW_MAXIMIZE';
+    wscSW_ShowNoActivate: Result := 'SW_SHOWNOACTIVATE';
+    wscSW_Show: Result := 'SW_SHOW';
+    wscSW_Minimize: Result := 'SW_MINIMIZE';
+    wscSW_ShowMinNoActive: Result := 'SW_SHOWMINNOACTIVE';
+    wscSW_ShowNA: Result := 'SW_SHOWNA';
+    wscSW_Restore: Result := 'SW_RESTORE';
+    wscSW_ShowDefault: Result := 'SW_SHOWDEFAULT';
+    wscSW_ForceMinimize: Result := 'SW_FORCEMINIMIZE, SW_MAX';
+  end;
+
+end;
+
+function TSWCmdItem.GetXmlText: string;
+begin
+  case FSWCmd of
+    wscSW_Hide: Result := 'SW_HIDE';
+    wscSW_ShowNormal: Result := 'SW_SHOWNORMAL';
+    wscSW_ShowMinimized: Result := 'SW_SHOWMINIMIZED';
+    wscSW_ShowMaximized: Result := 'SW_MAXIMIZED';
+    wscSW_ShowNoActivate: Result := 'SW_SHOWNOACTIVATE';
+    wscSW_Show: Result := 'SW_SHOW';
+    wscSW_Minimize: Result := 'SW_MINIMIZE';
+    wscSW_ShowMinNoActive: Result := 'SW_SHOWMINNOACTIVE';
+    wscSW_ShowNA: Result := 'SW_SHOWNA';
+    wscSW_Restore: Result := 'SW_RESTORE';
+    wscSW_ShowDefault: Result := 'SW_SHOWDEFAULT';
+    wscSW_ForceMinimize: Result := 'SW_FORCEMINIMIZE';
+  end;
+
+end;
+
+{ TWindowShowCommandList }
+
+procedure TWindowShowCommandList.AddItems;
+var
+  i:Integer;
+begin
+  Clear;
+
+  for i := 0 to EnumCount do begin
+    Add(TSWCmdItem.Create(TSWCmdEnum(i)));
+  end;
+
+end;
+
+function TWindowShowCommandList.EnumCount: Integer;
+ Var
+   count: Integer;
+   enum : TSWCmdEnum;
+ Begin
+   count := 0;
+   for enum := Low(TSWCmdEnum) to High(TSWCmdEnum) Do
+      Inc(count);
+
+   Result := count;
+end;
+
+function TWindowShowCommandList.GetItem(Index: Integer): TSWCmdItem;
+begin
+  if Index < Count then
+    Result := TSWCmdItem(Items[Index]);
+end;
+
+procedure TWindowShowCommandList.SetItem(Index: Integer;
+  const Value: TSWCmdItem);
+begin
+  if Index < Count then
+    Items[Index] := Value;
+end;
+
+end.
---
+++ Common/Units/TaskFilterList/TaskFilterList.pas
@@ -0,0 +1,313 @@
+unit TaskFilterList;
+
+interface
+
+uses
+  Windows,
+  Messages,
+  SysUtils,
+  Variants,
+  Classes,
+  Graphics,
+  Controls,
+  Contnrs,
+  Forms,
+  Dialogs,
+
+  JclSimpleXml,
+
+  SWCmdList, SharpApi;
+
+type
+  TFilterItemType = (fteSWCmd, fteWindow, fteProcess, fteCurrentMonitor,
+    fteCurrentVWM, fteMinimised);
+
+  TFilterItem = class(TPersistent)
+  public
+    Id: Integer;
+      Name: string;
+    FileName: string;
+    WndClassName: string;
+    FilterType: TFilterItemType;
+    SWCmds: TSWCmdEnums;
+    CurrentMonitor: Boolean;
+    CurrentVWM: Boolean;
+    Minimised: Boolean;
+
+    procedure Assign(AFilterItem: TPersistent); override;
+    constructor Create;
+  end;
+
+  TFilterItemList = class(TObjectList)
+  private
+    function GetItem(Index: Integer): TFilterItem;
+    procedure SetItem(Index: Integer; const Value: TFilterItem);
+  published
+  public
+    procedure Load;
+    procedure Save;
+    procedure Delete(AFilterItem: TFilterItem);
+
+    function AddFilename(AName, AFileName: string): TFilterItem;
+    function AddWndClass(AName, AWndClass: string): TFilterItem;
+    function AddMinimised(AName: string; AMinimised: Boolean): TFilterItem;
+    function AddCurrentVWM(AName: string; ACurrentVWM: Boolean): TFilterItem;
+    function AddCurrentMonitor(AName: string; ACurrentMonitor: Boolean): TFilterItem;
+    function AddSWCommands(AName: string; ASWCommands: TSWCmdEnums): TFilterItem;
+    function AddItem(AItem: TFilterItem): TFilterItem;
+
+    property Item[Index: Integer]: TFilterItem
+    read GetItem write SetItem; default;
+
+  end;
+
+implementation
+
+{ TFilterItemList }
+
+function TFilterItemList.AddItem(AItem: TFilterItem): TFilterItem;
+begin
+  Add(AItem);
+  AItem.Id := Self.Count;
+end;
+
+function TFilterItemList.AddCurrentMonitor(AName: string;
+  ACurrentMonitor: Boolean): TFilterItem;
+begin
+  Result := TFilterItem.Create;
+  Result.Name := AName;
+  Result.CurrentMonitor := ACurrentMonitor;
+  Result.FilterType := fteCurrentMonitor;
+
+  Add(Result);
+  Result.Id := Self.Count;
+end;
+
+function TFilterItemList.AddCurrentVWM(AName: string;
+  ACurrentVWM: Boolean): TFilterItem;
+begin
+  Result := TFilterItem.Create;
+  Result.Name := AName;
+  Result.CurrentVWM := ACurrentVWM;
+  Result.FilterType := fteCurrentVWM;
+
+  Add(Result);
+  Result.Id := Self.Count;
+end;
+
+function TFilterItemList.AddFilename(AName, AFileName: string): TFilterItem;
+begin
+  Result := TFilterItem.Create;
+  Result.Name := AName;
+  Result.FileName := AFileName;
+  Result.FilterType := fteProcess;
+
+  Add(Result);
+  Result.Id := Self.Count;
+end;
+
+function TFilterItemList.AddMinimised(AName: string; AMinimised: Boolean): TFilterItem;
+begin
+  Result := TFilterItem.Create;
+  Result.Name := AName;
+  Result.Minimised := AMinimised;
+  Result.FilterType := fteMinimised;
+
+  Add(Result);
+  Result.Id := Self.Count;
+end;
+
+function TFilterItemList.AddSWCommands(AName: string;
+  ASWCommands: TSWCmdEnums): TFilterItem;
+begin
+  Result := TFilterItem.Create;
+  Result.Name := AName;
+  Result.SWCmds := ASWCommands;
+  Result.FilterType := fteSWCmd;
+
+  Add(Result);
+  Result.Id := Self.Count;
+end;
+
+function TFilterItemList.AddWndClass(AName, AWndClass: string): TFilterItem;
+begin
+  Result := TFilterItem.Create;
+  Result.Name := AName;
+  Result.WndClassName := AWndClass;
+  Result.FilterType := fteWindow;
+
+  Add(Result);
+  Result.Id := Self.Count;
+end;
+
+procedure TFilterItemList.Delete(AFilterItem: TFilterItem);
+var
+  n:Integer;
+begin
+  n := IndexOf(AFilterItem);
+  if n <> -1 then Remove(AFilterItem);
+end;
+
+function TFilterItemList.GetItem(Index: Integer): TFilterItem;
+begin
+  Result := nil;
+
+  if Index < Count then
+    Result := TFilterItem(Items[index]);
+end;
+
+procedure TFilterItemList.Load;
+var
+  xml: TJclSimpleXML;
+  newItem: TFilterItem;
+  sFile, sName: string;
+  i, j: Integer;
+  elems: TJclSimpleXMLElems;
+  tmpCmds: TSWCmdEnums;
+  sList: TStringList;
+  showCmdsList: TWindowShowCommandList;
+begin
+  showCmdsList := TWindowShowCommandList.Create;
+  showCmdsList.AddItems;
+
+  Clear;
+  sFile := GetSharpeGlobalSettingsPath + 'SharpCore\Services\Shell\TaskFilters.xml';
+  if not (FileExists(sFile)) then exit;
+
+  sList := TStringList.Create;
+  xml := TJclSimpleXML.Create;
+  try
+    xml.LoadFromFile(sFile);
+    if xml.Root.Name = 'TaskFilters' then begin
+
+      for i := 0 to Pred(xml.Root.Items.Count) do begin
+
+        elems := xml.Root.Items.Item[i].Items;
+        sName := xml.Root.Items.Item[i].Properties.Value('Name');
+        case TFilterItemType(xml.Root.Items.Item[i].Properties.IntValue('Type')) of
+          fteSWCmd: begin
+              tmpCmds := [];
+              sList.CommaText := elems.Value('SW');
+
+              for j := 0 to Pred(showCmdsList.Count) do begin
+
+                if sList.IndexOf(showCmdsList[j].XmlText) <> -1 then
+                  Include(tmpCmds, showCmdsList[j].SWCmd);
+              end;
+
+              AddSWCommands(sName, tmpCmds);
+            end;
+          fteWindow: AddWndClass(sName, elems.Value('WndClassName'));
+          fteProcess: AddFilename(sName, elems.Value('FileName'));
+          fteCurrentMonitor: AddCurrentMonitor(sName, elems.BoolValue('CurrentMonitor', False));
+          fteCurrentVWM: AddCurrentVWM(sName, elems.BoolValue('CurrentVWM', False));
+          fteMinimised: AddMinimised(sName, elems.BoolValue('Minimised', False));
+        end;
+
+      end;
+    end;
+
+  finally
+    xml.Free;
+    showCmdsList.Free;
+    sList.Free;
+  end;
+end;
+
+procedure TFilterItemList.Save;
+var
+  sFile: string;
+  xml: TJclSimpleXML;
+  tmpFilter: TFilterItem;
+  i, iItem: Integer;
+  b: Boolean;
+  sList: TStringList;
+  showCmds: TWindowShowCommandList;
+begin
+  showCmds := TWindowShowCommandList.Create;
+  showCmds.AddItems;
+  sFile := GetSharpeGlobalSettingsPath + 'SharpCore\Services\Shell\TaskFilters.xml';
+  forceDirectories(ExtractFilePath(sFile));
+
+  sList := TStringList.Create;
+  xml := TJclSimpleXML.Create;
+  xml.Root.Name := 'TaskFilters';
+  try
+
+    for iItem := 0 to Pred(Count) do begin
+      tmpFilter := Item[iItem];
+      xml.Root.Items.Add('TaskFilter');
+      sList.Clear;
+
+      xml.Root.Items[iItem].Properties.Add('Name', tmpFilter.Name);
+      xml.Root.Items[iItem].Properties.Add('Type', Integer(tmpFilter.FilterType));
+
+      with xml.Root.Items[iItem].items do begin
+
+        case tmpFilter.FilterType of
+          fteSWCmd: begin
+              for i := 0 to Pred(showCmds.Count) do begin
+                if showCmds[i].SWCmd in tmpFilter.SWCmds then
+                  sList.Add(showCmds[i].XmlText);
+              end;
+              Add('SW', sList.CommaText);
+            end;
+          fteWindow: Add('WndClassName', tmpFilter.WndClassName);
+          fteProcess: Add('FileName', tmpFilter.FileName);
+          fteCurrentMonitor: Add('CurrentMonitor', tmpFilter.CurrentMonitor);
+          fteCurrentVWM: Add('CurrentVWM', tmpFilter.CurrentVWM);
+          fteMinimised: Add('Minimised', tmpFilter.Minimised);
+        end;
+      end;
+
+    end;
+
+  finally
+    xml.SaveToFile(sFile);
+    xml.Free;
+    showCmds.Free;
+    sList.Free;
+  end;
+end;
+
+procedure TFilterItemList.SetItem(Index: Integer; const Value: TFilterItem);
+begin
+  if Index < Count then
+    Item[Index] := Value;
+end;
+
+{ TFilterItem }
+
+procedure TFilterItem.Assign(AFilterItem: TPersistent);
+begin
+  if AFilterItem is TFilterItem then
+  begin
+    Name := TFilterItem(AFilterItem).Name;
+    FileName := TFilterItem(AFilterItem).FileName;
+    WndClassName := TFilterItem(AFilterItem).WndClassName;
+    FilterType := TFilterItem(AFilterItem).FilterType;
+
+    SWCmds := TFilterItem(AFilterItem).SWCmds;
+    CurrentMonitor := TFilterItem(AFilterItem).CurrentMonitor;
+    CurrentVWM := TFilterItem(AFilterItem).CurrentVWM;
+    Minimised := TFilterItem(AFilterItem).Minimised;
+  end
+  else
+    inherited Assign(AFilterItem);
+
+end;
+
+constructor TFilterItem.Create;
+begin
+  Name := '';
+  FileName := '';
+  WndClassName := '';
+  FilterType := fteSWCmd;
+  SWCmds := [];
+  CurrentMonitor := False;
+  CurrentVWM := False;
+  Minimised := False;
+end;
+
+end.
+
