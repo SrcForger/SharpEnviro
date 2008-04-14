c9f67af24013d54a9269d3218ff25e513ad2eb66
Billi Berserker <billiberserker@sharpe.gov>
2008-04-14 22:58:42 +0000

    * added suHotkey to update enum

--- Common/Libraries/SharpAPI/SharpAPI.pas
+++ Common/Libraries/SharpAPI/SharpAPI.pas
@@ -209,7 +209,7 @@ type
   TSU_UPDATE_ENUM = (suSkin, suSkinFileChanged, suScheme, suTheme, suIconSet,
     suBackground, suService, suDesktopIcon, suSharpDesk, suSharpMenu,
     suSharpBar, suCursor, suWallpaper, suDeskArea, suSkinFont, suDesktopObject,
-    suModule,suVWM,suCenter,suTaskFilter);
+    suModule, suVWM, suCenter, suTaskFilter, suHotkey);
   TSCE_EDITMODE_ENUM = (sceAdd, sceEdit, sceDelete);
   TSC_MODE_ENUM = (scmLive, scmApply);
 
