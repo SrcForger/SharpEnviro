4f142d3b73bd4ef5bda4f7118cc79de72b2242df
Lee Pixol <pixol@sharpe.gov>
2008-12-04 22:02:10 +0000

    * Converted plugin to use the TInterfacedSharpCenterHostBase interface

---
+++ Plugins/Configurations/Themes/SchemeEdit/SchemeEdit.dpr
@@ -0,0 +1,160 @@
+{
+Source Name: Scheme.dll
+Description: Configuration dll for Scheme settings
+Copyright (C) Martin Krämer <MartinKraemer@gmx.net>
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+SharpE Site
+http://www.sharpenviro.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+library SchemeEdit;
+
+uses
+  windows,
+  sysutils,
+  sharpapi,
+  classes,
+  SharpCenterApi,
+  ISharpCenterHostUnit,
+  ISharpCenterPluginUnit,
+  sharpthemeapi,
+  graphics,
+  uVistaFuncs,
+  forms,
+  GR32,
+  uEditWnd in 'uEditWnd.pas' {frmEditWnd};
+
+{$R *.RES}
+
+type
+  TSharpCenterPlugin = class( TInterfacedSharpCenterPlugin, ISharpCenterPluginPreview )
+  private
+    procedure Load;
+  public
+    constructor Create( APluginHost: TInterfacedSharpCenterHostBase );
+
+    function Open: Cardinal; override; stdcall;
+    procedure Close; override; stdcall;
+
+    function GetPluginDescriptionText: String; override; stdCall;
+    procedure Refresh; override; stdcall;
+
+
+    procedure Save; override; stdCall;
+    procedure UpdatePreview(ABitmap: TBitmap32); stdcall;
+
+  end;
+
+{ TSharpCenterPlugin }
+
+procedure TSharpCenterPlugin.Close;
+begin
+  FreeAndNil(frmEditWnd);
+end;
+
+constructor TSharpCenterPlugin.Create(APluginHost: TInterfacedSharpCenterHostBase);
+begin
+  PluginHost := APluginHost;
+end;
+
+function TSharpCenterPlugin.GetPluginDescriptionText: String;
+var
+  pluginId, themeId, schemeId: String;
+begin
+  {$REGION 'Get theme and scheme'}
+        pluginId := PluginHost.PluginId;
+        themeId := copy(pluginId, 0, pos(':',pluginId)-1);
+        schemeId := copy(pluginId, pos(':',pluginId)+1, length(pluginId) - pos(':',pluginId));
+    {$ENDREGION}
+
+  result := Format('Editing Scheme "%s"',[schemeId]);
+end;
+
+procedure TSharpCenterPlugin.Load;
+var
+  pluginId, themeId, schemeId: String;
+begin
+  {$REGION 'Get theme and scheme'}
+        pluginId := PluginHost.PluginId;
+        themeId := copy(pluginId, 0, pos(':',pluginId)-1);
+        schemeId := copy(pluginId, pos(':',pluginId)+1, length(pluginId) - pos(':',pluginId));
+    {$ENDREGION}
+
+  // Get Scheme Colors
+  frmEditWnd.Theme := themeId;
+  frmEditWnd.Scheme := schemeId;
+  XmlGetThemeScheme(themeId, schemeId, frmEditWnd.Colors);
+end;
+
+function TSharpCenterPlugin.Open: Cardinal;
+begin
+  if frmEditWnd = nil then frmEditWnd := TfrmEditWnd.Create(nil);
+  uVistaFuncs.SetVistaFonts(frmEditWnd);
+
+  frmEditWnd.PluginHost := PluginHost;
+  Load;
+  result := PluginHost.Open(frmEditWnd);
+end;
+
+procedure TSharpCenterPlugin.Refresh;
+begin
+  AssignThemeToForm(PluginHost.Theme,frmEditWnd);
+end;
+
+procedure TSharpCenterPlugin.Save;
+begin
+  XmlSetThemeScheme(frmEditWnd.Theme,frmEditWnd.Scheme,frmEditWnd.Colors);
+end;
+
+procedure TSharpCenterPlugin.UpdatePreview(ABitmap: TBitmap32);
+begin
+  if ( ( High(frmEditWnd.Colors)= 0) or (frmEditWnd = nil) ) then begin
+    ABitmap.SetSize(0,0);
+    exit;
+  end;
+
+  ABitmap.Clear(color32(0,0,0,0));
+  frmEditWnd.CreatePreviewBitmap(ABitmap);
+end;
+
+function GetMetaData(): TMetaData;
+begin
+  with result do
+  begin
+    Name := 'Edit Scheme';
+    Description := 'Configuration for editing a scheme';
+    Author := 'Lee Green (lee@sharpenviro.com)';
+    Version := '0.7.6.0';
+    DataType := tteConfig;
+    ExtraData := format('configmode: %d| configtype: %d',[Integer(scmApply),
+      Integer(suScheme)]);
+  end;
+end;
+
+
+function InitPluginInterface( APluginHost: TInterfacedSharpCenterHostBase ) : ISharpCenterPlugin;
+begin
+  result := TSharpCenterPlugin.Create(APluginHost);
+end;
+
+exports
+  InitPluginInterface,
+  GetMetaData;
+
+begin
+end.
---
+++ Plugins/Configurations/Themes/SchemeEdit/uEditWnd.pas
@@ -0,0 +1,195 @@
+﻿{
+Source Name: uSettingsWnd
+Description: Configuration window for Scheme Settings
+Copyright (C) Martin Krämer <MartinKraemer@gmx.net>
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+SharpE Site
+http://www.sharpenviro.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+unit uEditWnd;
+
+interface
+
+uses
+  Windows,
+  Messages,
+  SysUtils,
+  Variants,
+  Classes,
+  Graphics,
+  Controls,
+  Forms,
+  Dialogs,
+  StdCtrls,
+  ExtCtrls,
+  ComCtrls,
+  pngimage,
+  Mask,
+  ContNrs,
+  GR32_Image,
+  SharpApi,
+  SharpCenterApi,
+  SharpThemeApi,
+  SharpEListBoxEx,
+  PngImageList,
+  JvSimpleXML,
+  BarPreview,
+  Gr32,
+  ImgList, SharpESwatchManager, SharpEColorEditorEx, SharpEColorEditor, ISharpCenterHostUnit;
+
+type
+  TfrmEditWnd = class(TForm)
+    cexScheme: TSharpEColorEditorEx;
+    smScheme: TSharpESwatchManager;
+    tmrPreview: TTimer;
+
+    procedure RenderScheme;
+
+    procedure FormShow(Sender: TObject);
+    procedure cexSchemeChangeColor(ASender: TObject; AValue: Integer);
+    procedure cexSchemeUiChange(Sender: TObject);
+    procedure tmrPreviewTimer(Sender: TObject);
+  private
+    FTheme: string;
+    FScheme: string;
+    FPluginHost: TInterfacedSharpCenterHostBase;
+  public
+
+    Colors: TSharpEColorSet;
+    procedure CreatePreviewBitmap(var ABmp: TBitmap32);
+    property Theme: string read FTheme write FTheme;
+    property Scheme: string read FScheme write FScheme;
+
+    property PluginHost: TInterfacedSharpCenterHostBase read FPluginHost write FPluginHost;
+  end;
+
+var
+  frmEditWnd: TfrmEditWnd;
+
+implementation
+
+uses
+  JclStrings,
+  SharpFx;
+
+{$R *.dfm}
+
+procedure TfrmEditWnd.cexSchemeChangeColor(ASender: TObject;
+  AValue: Integer);
+begin
+
+  if TSharpEColorEditorExItem(ASender).ValueEditorType = vetColor then
+    Colors[TSharpEColorEditorExItem(ASender).Tag].Color := AValue
+  else
+    Colors[TSharpEColorEditorExItem(ASender).Tag].Color := AValue;
+
+  tmrPreview.Enabled := True;
+end;
+
+procedure TfrmEditWnd.cexSchemeUiChange(Sender: TObject);
+begin
+  FPluginHost.SetSettingsChanged;
+end;
+
+procedure TfrmEditWnd.CreatePreviewBitmap(var ABmp: TBitmap32);
+var
+  bmp: TBitmap32;
+begin
+  if FScheme = '' then begin
+    ABmp.SetSize(0, 0);
+    exit;
+  end;
+
+  bmp := TBitmap32.Create;
+  try
+
+    BarPreview.CreateBarPreview(bmp, FTheme, XmlGetSkin(FTheme),
+      FScheme,150,Colors, true);
+
+    ABmp.SetSize(bmp.Width, bmp.height);
+    Bmp.DrawTo(ABmp);
+  finally
+    bmp.Free;
+  end;
+end;
+
+procedure TfrmEditWnd.FormShow(Sender: TObject);
+begin
+  Self.DoubleBuffered := True;
+  cexScheme.DoubleBuffered := True;
+  Self.ParentBackground := False;
+  RenderScheme;
+end;
+
+procedure TfrmEditWnd.RenderScheme;
+var
+  color: TSharpESkinColor;
+  item: TSharpEColorEditorExItem;
+  i, h: Integer;
+begin
+
+  if FScheme = '' then exit;
+
+  // Populate
+  cexScheme.BeginUpdate;
+  cexScheme.Items.Clear;
+  try
+    for i := 0 to high(Colors) do begin
+      color := Colors[i];
+      item := TSharpEColorEditorExItem.Create(cexScheme.Items);
+
+      item.Title := color.Name;
+      item.ColorCode := color.Color;
+      item.Tag := i;
+
+      if color.schemetype = stInteger then
+        item.ValueEditorType := vetValue
+      else
+        item.ValueEditorType := vetColor;
+
+      item.Value := color.Color;
+      item.Description := color.Info + ':';
+      item.ValueText := color.Name;
+    end;
+  finally
+    cexScheme.EndUpdate;
+  end;
+
+{$REGION 'Update Size'}
+  h := 0;
+  if high(Colors) <> 0 then
+    h := cexScheme.Items.Item[0].ColorEditor.CollapseHeight * high(Colors)
+      + cexScheme.Items.Item[0].ColorEditor.ExpandedHeight;
+
+  cexScheme.Height := h + 10;
+  self.Height := h + 10;
+  PluginHost.Refresh;
+{$ENDREGION}
+
+end;
+
+procedure TfrmEditWnd.tmrPreviewTimer(Sender: TObject);
+begin
+  tmrPreview.Enabled := false;
+  PluginHost.Refresh(rtPreview);
+
+end;
+
+end.
+
