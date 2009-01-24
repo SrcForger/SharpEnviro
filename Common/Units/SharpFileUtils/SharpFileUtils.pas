bb2f94fa66835bbe42e57fc12524f1e17b14644f
Billi Berserker <billiberserker@sharpe.gov>
2009-01-24 00:32:17 +0000

    * added FindFiles method

--- Common/Units/SharpFileUtils/SharpFileUtils.pas
+++ Common/Units/SharpFileUtils/SharpFileUtils.pas
@@ -32,9 +32,52 @@ uses
   SysUtils,
   JclStrings;
 
+procedure FindFiles(var FilesList: TStringList; StartDir, FileMask: string);
 function GetFileNameWithoutParams(pTarget : String) : String;
 
 implementation
+
+procedure FindFiles(var FilesList: TStringList; StartDir, FileMask: string);
+var
+  SR: TSearchRec;
+  DirList: TStringList;
+  IsFound: Boolean;
+  i: integer;
+begin
+  if StartDir[length(StartDir)] <> '\' then
+    StartDir := StartDir + '\';
+
+  { Build a list of the files in directory StartDir
+     (not the directories!)                         }
+
+  IsFound := FindFirst(StartDir + FileMask, faAnyFile - faDirectory, SR) = 0;
+  while IsFound do begin
+    FilesList.Add(StartDir + SR.Name);
+    IsFound := FindNext(SR) = 0;
+  end;
+  SysUtils.FindClose(SR);
+
+  // Build a list of subdirectories
+  DirList := TStringList.Create;
+  try
+    IsFound := FindFirst(StartDir + '*.*', faAnyFile, SR) = 0;
+    while IsFound do begin
+      if ((SR.Attr and faDirectory) <> 0) and
+        (SR.Name[1] <> '.') then
+        DirList.Add(StartDir + SR.Name);
+      IsFound := FindNext(SR) = 0;
+    end;
+    SysUtils.FindClose(SR);
+
+    // Scan the list of subdirectories
+    for i := 0 to DirList.Count - 1 do
+      FindFiles(FilesList, DirList[i], FileMask);
+
+  finally
+    DirList.Free;
+  end;
+
+end;
 
 function GetFileNameWithoutParams(pTarget : String) : String;
 var
