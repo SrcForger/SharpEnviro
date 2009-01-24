1ef278b11250fd85c0b18f018b679c37c55090df
Lee Pixol <pixol@sharpe.gov>
2009-01-24 18:03:34 +0000

    * Fixed hotkeys not loading at startup

--- Plugins/Services/Hotkeys/uHotkeyServiceMain.pas
+++ Plugins/Services/Hotkeys/uHotkeyServiceMain.pas
@@ -89,8 +89,6 @@ begin
   fn := GetSharpeUserSettingsPath + cSettingsLocation;
   If Not(Assigned(FHotkeyList)) Then
     FHotkeyList := THotkeyList.Create;
-  FHotkeyList.FileName := fn;
-  FHotkeyList.Load;
 
   // Add Hotkeys to Manager
   Debug(Format('Hotkey data Items: %d',[FHotkeyList.Count]),DMT_STATUS);
