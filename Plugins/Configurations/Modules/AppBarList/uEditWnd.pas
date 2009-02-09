9e53c393b73d1c71a3f9cfd40ba174c503eaa9b9
Billi Berserker <billiberserker@sharpe.gov>
2009-02-09 22:49:27 +0000

    * replaced SharpDialogs call with custom menu (which only allows opening .exe files and picking a target from a process/window list) * fixed Tab Order in edit Window

--- Plugins/Configurations/Modules/AppBarList/uEditWnd.pas
+++ Plugins/Configurations/Modules/AppBarList/uEditWnd.pas
@@ -48,9 +48,11 @@ uses
   // Jedi
   JvExControls,
   JvComponentBase,
+  JclSysInfo,
 
   // PngImage
-  PngImageList, Buttons, PngSpeedButton, StdCtrls, ExtCtrls, SharpEListBoxEx, uAppBarList, SharpDialogs;
+  PngImageList, Buttons, PngSpeedButton, StdCtrls, ExtCtrls, SharpEListBoxEx, uAppBarList, SharpDialogs,
+  Menus, Dialogs;
 
 type
   TfrmEdit = class(TForm)
@@ -59,18 +61,26 @@ type
     btnCommandBrowse: TPngSpeedButton;
     edIcon: TLabeledEdit;
     btnIconBrowse: TPngSpeedButton;
+    targetPopup: TPopupMenu;
+    File1: TMenuItem;
+    RunningProcess1: TMenuItem;
+    Open1: TMenuItem;
+    ilWndClass: TPngImageList;
+    OpenApplication: TOpenDialog;
 
     procedure UpdateEditState(Sender: TObject);
     procedure btnCommandBrowseClick(Sender: TObject);
     procedure btnIconBrowseClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-
+    procedure EnumWindowsPopupClick(Sender: TObject);
+    procedure Open1Click(Sender: TObject);
   private
     { Private declarations }
     FUpdating: Boolean;
     FItemEdit: TAppBarItem;
     FPluginHost: TInterfacedSharpCenterHostBase;
+    procedure BuildWindowList;    
   public
     { Public declarations }
     
@@ -125,16 +135,16 @@ begin
   end;
 end;
 
-procedure TfrmEdit.btnCommandBrowseClick(Sender: TObject);
-var
-  s: string;
+procedure TfrmEdit.Open1Click(Sender: TObject);
 begin
-  s := SharpDialogs.TargetDialog([stiFile,stiRecentFiles,stiMostUsedFiles],
-    ClientToScreen(point(btnCommandBrowse.Left, btnCommandBrowse.Top)));
-  if length(trim(s)) > 0 then
-  begin
-    edCommand.Text := s;
-  end;
+  if OpenApplication.Execute then
+    if FileExists(OpenApplication.FileName) then
+      edCommand.Text := OpenApplication.FileName;
+end;
+
+procedure TfrmEdit.btnCommandBrowseClick(Sender: TObject);
+begin
+  BuildWindowList;
 end;
 
 procedure TfrmEdit.btnIconBrowseClick(Sender: TObject);
@@ -148,6 +158,130 @@ begin
   begin
     edIcon.Text := s;
   end;
+end;
+
+function GetWndClass(wnd: hwnd): string;
+var
+  buf: array[0..127] of Char;
+begin
+  GetClassName(wnd, buf, SizeOf(buf));
+  result := buf;
+end;
+
+function GetCaption(wnd: hwnd): string;
+var
+  buf: array[0..1024] of char;
+begin
+  GetWindowText(wnd, @buf, sizeof(buf));
+  result := buf;
+end;
+
+function GetIcon(wnd: hwnd): hicon;
+const
+  ICON_SMALL2 = 2;
+var
+  newicon: hicon;
+begin
+  newicon := 0;
+
+  SendMessageTimeout(wnd, WM_GETICON, ICON_SMALL, 0, SMTO_ABORTIFHUNG or SMTO_BLOCK, 50, DWORD(newicon));
+  if newicon = 0 then newicon := HICON(GetClassLong(wnd, GCL_HICONSM));
+  if newicon = 0 then SendMessageTimeout(wnd, WM_GETICON, ICON_SMALL2, 0, SMTO_ABORTIFHUNG or SMTO_BLOCK, 50, DWORD(newicon));
+  result := newicon;
+end;
+
+procedure TfrmEdit.EnumWindowsPopupClick(Sender: TObject);
+begin
+  if not (Sender is TMenuItem) then exit;
+  frmEdit.edCommand.Text := GetProcessNameFromWnd(TMenuItem(Sender).Tag);
+end;
+
+procedure TfrmEdit.BuildWindowList;
+
+  function EnumWindowsProc(wnd: HWND; lparam: LPARAM): bool; stdcall;
+  var
+    item: TMenuItem;
+    tempIcon: TIcon;
+    icon : hIcon;
+    validicon : boolean;
+    sWndClass, sCaption, sProcName, s: string;
+  begin
+    if (GetWindowLong(Wnd, GWL_STYLE) and WS_SYSMENU <> 0) and
+      ((IsWindowVisible(Wnd) or IsIconic(wnd)) and
+      (GetWindowLong(Wnd, GWL_STYLE) and WS_CHILD = 0) and
+      (GetWindowLong(Wnd, GWL_EXSTYLE) and WS_EX_TOOLWINDOW = 0)) then
+    begin
+      item := TMenuItem.Create(frmEdit.RunningProcess1);
+      item.Tag := wnd;
+
+{$REGION 'GetWndClass'}
+      sWndClass := '';
+      s := GetWndClass(wnd);
+      if s <> '' then begin
+        if length(s) > 30 then begin
+          SetLength(s, 27);
+          s := s + '...';
+        end;
+        sWndClass := Format('%s -', [s]);
+      end;
+{$ENDREGION}
+
+{$REGION 'GetCaption'}
+      sCaption := '';
+      s := GetCaption(wnd);
+      if s <> '' then begin
+        if length(s) > 30 then begin
+          SetLength(s, 27);
+          s := s + '...';
+        end;
+        sCaption := Format(' (%s)', [s]);
+      end;
+{$ENDREGION}
+
+{$REGION 'GetProcessName'}
+      sProcName := '';
+      s := ExtractFileName(GetProcessNameFromWnd(wnd));
+      if s <> '' then begin
+        if length(s) > 30 then begin
+          SetLength(s, 27);
+          s := s + '...';
+        end;
+        sProcName := Format(' [%s]', [s]);
+      end;
+{$ENDREGION}
+
+      item.Caption := Format('%s%s%s', [sWndClass, sCaption, sProcName]);
+      item.ImageIndex := frmEdit.ilWndClass.Count;
+      item.OnClick := EnumWindowsPopupClick;
+      frmEdit.RunningProcess1.add(item);
+      validicon := True;
+      tempIcon := TIcon.Create;
+      try
+        icon := GetIcon(wnd);
+        if icon <> 0 then
+          tempIcon.Handle := icon
+        else validicon := False;
+        if validicon then
+          validicon := (tempIcon.Width <= 16) and (tempIcon.Width > 0);
+        if validicon then
+          frmEdit.ilWndClass.AddIcon(tempIcon)
+        else item.ImageIndex := 3;
+      finally
+        tempIcon.Free;
+      end;
+    end;
+    result := True;
+  end;
+
+var
+  n : integer;
+
+begin
+  for n := ilWndClass.Count - 1 downto 4 do
+    ilWndClass.Delete(n);
+  RunningProcess1.clear;
+  EnumWindows(@EnumWindowsProc, 0);
+  targetPopup.Popup(Mouse.CursorPos.X, Mouse.CursorPos.Y);
 end;
 
 procedure TfrmEdit.FormCreate(Sender: TObject);
