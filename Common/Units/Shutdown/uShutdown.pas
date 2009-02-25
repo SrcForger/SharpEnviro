7cc295d6195079bee9e0efeac3d026c676405772
Lee Pixol <pixol@sharpe.gov>
2009-02-25 22:40:32 +0000

    * Added Verbose property option, and handling * Changed lock workstation to use windows api

--- Common/Units/Shutdown/uShutdown.pas
+++ Common/Units/Shutdown/uShutdown.pas
@@ -34,24 +34,28 @@ uses
 
 type
   TShutdownActionType = (sdPowerOff, sdShutdown, sdReboot, sdLogOff,
-    sdSuspend, sdHibernate);
+    sdLock, sdHibernate);
 
   TQueryShutdownEvent = procedure(Sender: TObject;
     var CanShutdown: Boolean) of object;
 
 type
-  TSEShutDown = class(TObject)
+  TSEShutDown = class
   private
 
     FActionType: TShutdownActionType;
     FForce: Boolean;
     FOnQueryShutdown: TQueryShutdownEvent;
-
+    FVerbose: Boolean;
+    FParentHandle: THandle;
+    function ShowConfirmation: Boolean;
+    function GetParentHandle: THandle;
+    function GetShutdownTypeAsString: string;
   protected
     procedure DoQueryShutdown(var CanShutdown: Boolean); virtual;
 
   public
-    constructor Create; reintroduce;
+    constructor Create(AParentHandle: THandle);
     function Execute: Boolean; virtual;
 
     property ActionType: TShutDownActionType read FActionType
@@ -60,17 +64,23 @@ type
 
     property OnQueryShutdown: TQueryShutdownEvent read FOnQueryShutdown
       write FOnQueryShutdown;
+
+    property Verbose: Boolean read FVerbose write FVerbose;
+    property ParentHandle: THandle read GetParentHandle;
   end;
 
 implementation
 
 { TScShutDown }
 
-constructor TSEShutDown.Create;
+constructor TSEShutDown.Create(AParentHandle: THandle);
 begin
   FActionType := sdPowerOff;
   FForce := False;
-  inherited;
+  FVerbose := True;
+  FParentHandle := AParentHandle;
+
+  inherited Create;
 end;
 
 procedure TSEShutDown.DoQueryShutdown(var CanShutdown: Boolean);
@@ -87,6 +97,11 @@ var
   CanShutdown: Boolean;
 begin
   Result := False;
+
+  // Show modal confirmation if enabled verbosity
+  if not(ShowConfirmation) then
+    Exit;
+
   if Win32Platform = VER_PLATFORM_WIN32_NT then begin
     hProcess := OpenProcess(PROCESS_ALL_ACCESS, True, GetCurrentProcessID);
     try
@@ -122,9 +137,45 @@ begin
     sdShutdown: Result := ExitWindowsEx(Flags or EWX_SHUTDOWN, 0);
     sdReboot: Result := ExitWindowsEx(Flags or EWX_REBOOT, 0);
     sdLogoff: Result := ExitWindowsEx(Flags or EWX_LOGOFF, 0);
-    sdSuspend: Result := SetSystemPowerState(True, FForce);
+    sdLock: Windows.LockWorkStation;
     sdHibernate: Result := SetSystemPowerState(False, FForce);
   end;
+end;
+
+function TSEShutDown.GetParentHandle: THandle;
+begin
+  Result := FParentHandle;
+end;
+
+function TSEShutDown.GetShutdownTypeAsString: string;
+begin
+  case FActionType of
+    sdPowerOff: result := 'power off';
+    sdShutdown: result := 'shut down' ;
+    sdReboot: result := 'reboot';
+    sdLogOff: result := 'log off';
+    sdLock: result := 'lock' ;
+    sdHibernate: result := 'hibernate';
+  end;
+end;
+
+function TSEShutDown.ShowConfirmation: Boolean;
+var
+  sTitle, sDescription, shutdownType: string;
+begin
+  if ( not(FVerbose) or (FActionType = sdLock)) then begin
+    Result := true;
+    exit;
+  end;
+
+  result := false;
+  shutdownType := GetShutdownTypeAsString;
+  sTitle := format('Do you really want to %s your computer?',[shutdownType]);
+  sDescription := format('Confirm %s',[shutdownType]);
+
+  if windows.MessageBox(FParentHandle, PChar(sTitle),
+          PChar(sDescription), MB_YESNO or MB_ICONQUESTION or MB_APPLMODAL or MB_TOPMOST) = IDOK then
+            result := true;
 end;
 
 end.
