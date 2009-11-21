e3fb0d0062f4c3edb920ba6eb2109e919bf8f205
Mathias Tillman <homersp@sharpe.gov>
2009-11-21 00:06:38 +0000

    * The Center window is now brought to the foreground if you try to open another instance of the program

--- Components/SharpCenter/SharpCenter.dpr
+++ Components/SharpCenter/SharpCenter.dpr
@@ -20,32 +20,35 @@ uses
 {$R *.res}
 {$R metadata.res}
 
-procedure CheckMutex(var Terminate: Boolean);
+function CheckMutex: Boolean;
 var
   MutexHandle:THandle;
 begin
-  Terminate := False;
+  Result := False;
   MutexHandle := CreateMutex(nil, TRUE, Pchar('SharpCenterMutexX'));
   if MutexHandle <> 0 then begin
     if GetLastError = ERROR_ALREADY_EXISTS then begin
       CloseHandle(MutexHandle);
-      Terminate := True;
+      Result := True;
       Exit;
     end;
   end;
 end;
 
 var
-  exit:Boolean;
+  CenterWnd : HWND;
 begin
   Application.Initialize;
   {$IFDEF VER185} Application.MainFormOnTaskBar := True; {$ENDIF}
 
-  exit := False;
-  CheckMutex(exit);
-  if exit then Application.Terminate else begin
-
-  Application.CreateForm(TSharpCenterWnd, SharpCenterWnd);
-  Application.Run;
+  if CheckMutex then
+  begin
+    CenterWnd := FindWindow('TSharpCenterWnd', 'SharpCenter');
+    SetForegroundWindow(CenterWnd);
+    Application.Terminate;
+  end else
+  begin
+    Application.CreateForm(TSharpCenterWnd, SharpCenterWnd);
+    Application.Run;
   end;
  end.
