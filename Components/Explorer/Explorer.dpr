73c82a77b208f1faaaefb50dde2225852621feb8
Brum 74 <brum74@sharpe.gov>
2010-02-04 06:17:13 +0000

    * Create a Mutex for SharpExplorer to ensure we only have 1, also pass subsequent calls to the windows explorer.exe.

--- Components/Explorer/Explorer.dpr
+++ Components/Explorer/Explorer.dpr
@@ -4,14 +4,40 @@ uses
   Windows,
   ActiveX,
   Forms,
+  SysUtils,
   uSharpExplorerForm in 'uSharpExplorerForm.pas' {ExplorerForm},
   SharpAPI in '..\..\Common\Libraries\SharpAPI\SharpAPI.pas';
 
 {$R *.res}
 {$R metadata.res}
 
+var
+  hMutex: THandle;
+  winDir : PAnsiChar;
+  cmdToExec : string;
+
 begin
   CoInitialize(nil);
+
+  // We are creating a mutex because we only want 1 of our Explorer.exe processes.
+  // For any subsequent attempts we will assume that the windows explorer.exe was
+  // meant to be run and pass along any arguments.
+  hMutex := CreateMutex(nil, TRUE, 'SharpExplorer');
+  if hMutex <> 0 then
+    if GetLastError = ERROR_ALREADY_EXISTS then
+    begin
+      CloseHandle(hMutex);
+      // Get the path to explorer.exe in the windows directory
+      // so that we can launch it instead.
+      winDir := StrAlloc(MAX_PATH);
+      GetWindowsDirectory(winDir, MAX_PATH);
+      cmdToExec := IncludeTrailingBackslash(winDir) + 'explorer.exe';
+      if ParamCount > 0 then
+        cmdToExec := cmdToExec + ' ' + CmdLine;
+      SharpExecute(cmdToExec);
+      StrDispose(winDir);
+      Exit;
+    end;
 
   Application.Initialize;
   Application.Title := 'SharpExplorer';
