33075cb20b3fe3c60c7cdd8102b95b9a530e6bf6
Mathias Tillman <homersp@sharpe.gov>
2010-02-25 23:25:44 +0000

    * Fixes

--- Common/Interfaces/ISharpCenterPluginUnit.pas
+++ Common/Interfaces/ISharpCenterPluginUnit.pas
@@ -16,57 +16,72 @@ const
 type
   ISharpCenterPluginValidation = interface(IInterface)
   ['{3E12792D-C1D9-4C58-B055-FFC4B1A72D3F}']
-
     procedure SetupValidators; stdCall;
   end;
 
   ISharpCenterPluginPreview = interface(IInterface)
   ['{C397A7F1-86D1-4E20-AD5C-EA52C51DC306}']
-
-  procedure UpdatePreview( ABitmap: TBitmap32 ); stdCall;
+    procedure UpdatePreview( ABitmap: TBitmap32 ); stdCall;
   end;
 
   ISharpCenterPluginEdit = interface(IInterface)
   ['{C7477868-4A58-4EDF-8D81-2C0B1732C116}']
-
-  function OpenEdit: THandle; stdCall;
-  procedure CloseEdit(AApply:Boolean); stdCall;
+    function OpenEdit: THandle; stdCall;
+    procedure CloseEdit(AApply:Boolean); stdCall;
   end;
 
   ISharpCenterPluginTabs = interface(IInterface)
   ['{1874CB85-3577-4CBD-9F73-EE464223E3FD}']
-
-  procedure ClickPluginTab(ATab: TStringItem); stdCall;
-  procedure AddPluginTabs(ATabItems: TStringList); stdCall;
+    procedure ClickPluginTab(ATab: TStringItem); stdCall;
+    procedure AddPluginTabs(ATabItems: TStringList); stdCall;
   end;
 
   ISharpCenterPlugin = interface(IInterface)
   ['{F9E1BB12-4885-43FF-B4CB-56517F6F486D}']
-
-  function GetPluginHost : ISharpCenterHost; stdcall;
-  procedure SetPluginHost(Value : ISharpCenterHost); stdcall;
-  property PluginHost: ISharpCenterHost read GetPluginHost write SetPluginHost;
-
-  function Open : THandle; stdcall;
-  procedure Close; stdcall;
-  procedure Save; stdcall;
-  procedure Refresh(Theme : TCenterThemeInfo; AEditing : Boolean); stdCall;
-
-  function GetPluginName : string; stdcall;
-  function GetPluginStatusText : string; stdcall;
-  function GetPluginDescriptionText : string; stdcall;
-  end;
-
-  TInterfacedSharpCenterPlugin = class(TInterfacedObject, ISharpCenterPlugin)
-
-  private
-    FPluginHost: ISharpCenterHost;
+    // ISharpCenterPlugin
     function GetPluginHost : ISharpCenterHost; stdcall;
     procedure SetPluginHost(Value : ISharpCenterHost); stdcall;
+    property PluginHost: ISharpCenterHost read GetPluginHost write SetPluginHost;
+
+    function GetCanDestroy : boolean; stdcall;
+    procedure SetCanDestroy(Value : boolean); stdcall;
+    property CanDestroy: boolean read GetCanDestroy write SetCanDestroy;
+
+    function Open : THandle; stdcall;
+    procedure Close; stdcall;
+    procedure Save; stdcall;
+    procedure Refresh(Theme : TCenterThemeInfo; AEditing : Boolean); stdCall;
+
+    function GetPluginName : string; stdcall;
+    function GetPluginStatusText : string; stdcall;
+    function GetPluginDescriptionText : string; stdcall;
+  end;
+
+  TInterfacedSharpCenterPlugin = class(TObject, ISharpCenterPlugin)
+  private
+    FRefCount: integer;
+    FCanDestroy: boolean;
+    FPluginHost: ISharpCenterHost;
+
+    function GetPluginHost : ISharpCenterHost; stdcall;
+    procedure SetPluginHost(Value : ISharpCenterHost); stdcall;
+
+    function GetCanDestroy : boolean; stdcall;
+    procedure SetCanDestroy(Value : boolean); stdcall;
+  protected
+    // IUnknown
+    function _AddRef: Integer; virtual; stdcall;
+    function _Release: Integer; virtual; stdcall;
+    
   public
     destructor Destroy; override;
 
+    // IUnknown
+    function QueryInterface(const IID: TGUID; out Obj): HResult; virtual; stdcall;
+
+    // ISharpCenterPlugin
     property PluginHost: ISharpCenterHost read GetPluginHost write SetPluginHost;
+    property CanDestroy: boolean read FCanDestroy write FCanDestroy;
 
     function Open : THandle; virtual; stdcall;
     procedure Close; virtual; stdcall;
@@ -85,6 +100,39 @@ implementation
 
 { TInterfacedSharpCenterPlugin }
 
+function TInterfacedSharpCenterPlugin._AddRef: Integer;
+begin
+  Result := InterlockedIncrement(FRefCount);
+end;
+
+function TInterfacedSharpCenterPlugin._Release: Integer;
+begin
+  Result := InterlockedDecrement(FRefCount);
+  if (Result = 0) and (FCanDestroy) then
+    Destroy;
+end;
+
+function TInterfacedSharpCenterPlugin.QueryInterface(const IID: TGUID; out Obj): HResult;
+const
+  E_NOINTERFACE = HResult($80004002);
+begin
+  FRefCount := 0;
+
+  if GetInterface(IID, Obj) then
+    Result := 0
+  else
+    Result := E_NOINTERFACE;
+end;
+
+function TInterfacedSharpCenterPlugin.GetCanDestroy : boolean;
+begin
+  Result := FCanDestroy;
+end;
+procedure TInterfacedSharpCenterPlugin.SetCanDestroy(Value : boolean);
+begin
+  FCanDestroy := Value;
+end;
+
 procedure TInterfacedSharpCenterPlugin.Close;
 begin
 
@@ -99,7 +147,7 @@ end;
 
 function TInterfacedSharpCenterPlugin.GetPluginDescriptionText: string;
 begin
-
+  Result := '';
 end;
 
 function TInterfacedSharpCenterPlugin.GetPluginHost: ISharpCenterHost;
@@ -109,12 +157,12 @@ end;
 
 function TInterfacedSharpCenterPlugin.GetPluginName: string;
 begin
-
+  Result := '';
 end;
 
 function TInterfacedSharpCenterPlugin.GetPluginStatusText: string;
 begin
-
+  Result := '';
 end;
 
 function TInterfacedSharpCenterPlugin.Open: THandle;
