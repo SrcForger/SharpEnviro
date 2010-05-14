61892e93a2f5a84c5710e6951219a983aa4f4c6d
Martin <martink84@sharpe.gov>
2010-05-14 08:10:27 +0000

    * added GPL Header * added restoring of the backup file (-> failed to load original file -> backup worked -> copy backup to original file)

--- Common/Units/XML/uSharpXMLUtils.pas
+++ Common/Units/XML/uSharpXMLUtils.pas
@@ -1,3 +1,28 @@
+{
+Source Name: uSharpXMLUtils.dpr
+Description: XML Loading and Saving functions including backup
+Copyright (C) Martin Krämer <MartinKraemer@gmx.net>
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+SharpE Site
+http://www.sharpenviro.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
 unit uSharpXMLUtils;
 
 interface
@@ -20,7 +45,16 @@ function LoadXMLFromSharedFile(var XML : TJclSimpleXML; filename : string; Backu
 begin
   result := LoadXMLFromSharedFile(XML,filename);
   if (BackupIfFailed) and (not result) then
+  begin
     result := LoadXMLFromSharedFile(XML,filename + '~');
+    if result then // failed to original file, but backup worked
+    begin          // copy backup over to normal file                                                    
+      try
+        CopyFile(PChar(filename + '~'),PChar(filename),false);
+      except
+      end;
+    end;
+  end;
 end;
 
 function LoadXMLFromSharedFile(var XML : TJclSimpleXML; filename : string) : boolean;
