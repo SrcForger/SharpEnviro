961b160b805f635e7f90edb20e6784ababa1fc15
Brum 74 <brum74@sharpe.gov>
2010-05-31 01:25:57 +0000

    * changed version number to 0.8.0.0 * updated use new .8.0 graphic

--- Plugins/Configurations/Components/Home/Home.dpr
+++ Plugins/Configurations/Components/Home/Home.dpr
@@ -125,7 +125,7 @@ begin
     Name := 'Home';
     Description := tmp;
     Author := 'Lee Green (lee@sharpenviro.com)';
-    Version := '0.7.6.5';
+    Version := '0.8.0.0';
     DataType := tteConfig;
     ExtraData := format('configmode: %d| configtype: %d',[Integer(scmLive),
       Integer(suCenter)]);
