2841fd230136f0d9061e1aad3e8efe296ec8bbab
Martin <martink84@sharpe.gov>
2010-06-07 15:29:50 +0000

    * fixed icon showing when display style is set to Text only

--- Plugins/Modules/Notes/MainWnd.pas
+++ Plugins/Modules/Notes/MainWnd.pas
@@ -126,8 +126,11 @@ begin
     exit;
 
   size := GetNearestIconSize(mInterface.SkinInterface.SkinManager.Skin.Button.Normal.Icon.Dimension.Y);
-  if not IconStringToIcon('icon.notes','',Button.Glyph32,size) then
-       Button.Glyph32.SetSize(0,0);
+  if Settings.ShowIcon then
+  begin
+    if not IconStringToIcon('icon.notes','',Button.Glyph32,size) then
+      Button.Glyph32.SetSize(0,0);
+  end else Button.Glyph32.SetSize(0,0);
   Button.Repaint;
 end;
 
