673ab3047dae9d283974c09bcc873e5a813ef577
Brum 74 <brum74@sharpe.gov>
2010-07-17 16:01:48 +0000

    * updated SharpCompile to include metadata.rc and VersionInfo.rc * added missing VersionInfo.rc files from previous check-in

--- Components/SharpCompile/SharpCompile.dpr
+++ Components/SharpCompile/SharpCompile.dpr
@@ -1,15 +1,15 @@
 program SharpCompile;
 
+{$R 'metadata.res' 'metadata.rc'}
+{$R 'VersionInfo.res' 'VersionInfo.rc'}
+{$R *.res}
+
 uses
-//  VCLFixPack,
   Forms,
   SharpApi,
-  {$IFDEF DEBUG}DebugDialog in '..\..\Common\Units\DebugDialog\DebugDialog.pas',{$ENDIF}
+  DebugDialog in '..\..\Common\Units\DebugDialog\DebugDialog.pas',
   fMain in 'fMain.pas' {SharpCompileMainWnd},
   uCompiler in 'uCompiler.pas';
-
-{$R *.res}
-{$R metadata.res}
 
 begin
   Application.Initialize;
