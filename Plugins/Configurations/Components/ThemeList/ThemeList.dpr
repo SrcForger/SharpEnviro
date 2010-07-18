26f9111d6015707c31bfba7d52e31bff7ea9a756
Brum 74 <brum74@sharpe.gov>
2010-07-18 18:45:39 +0000

    * removed rc files from projects as they don't get recompiled with MSBuild * added PreBuildEvent to delete the res files and compile the rc files with brcc32

--- Plugins/Configurations/Components/ThemeList/ThemeList.dpr
+++ Plugins/Configurations/Components/ThemeList/ThemeList.dpr
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 library ThemeList;
 
-{$R 'ThemeListGlyphs.res' 'ThemeListGlyphs.rc'}
+{$R 'ThemeListGlyphs.res'}
 {$R *.res}
 
 uses
