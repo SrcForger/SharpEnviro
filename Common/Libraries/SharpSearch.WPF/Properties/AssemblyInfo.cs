46b8181ea1e39fd057b31940b76ef50c1fdd2cde
Brum 74 <brum74@sharpe.gov>
2010-10-23 16:56:37 +0000

    * updated c# version info to consistent with delphi version info * default FileVersion and Version to 0.0.0.0

--- Common/Libraries/SharpSearch.WPF/Properties/AssemblyInfo.cs
+++ Common/Libraries/SharpSearch.WPF/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpSearch.WPF")]
+[assembly: AssemblyTitle("SharpEnviro Search WPF Common Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SharpSearch.WPF")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("SharpEnviro")]
+[assembly: AssemblyProduct("SharpEnviro")]
+[assembly: AssemblyCopyright("SharpEnviro Development Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
--- Components/SharpLinkLauncherNET/Properties/AssemblyInfo.cs
+++ Components/SharpLinkLauncherNET/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpLinkLauncherNET")]
+[assembly: AssemblyTitle("SharpEnviro Link Launcher Component")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SharpEnviro")]
-[assembly: AssemblyProduct("SharpLinkLauncherNET")]
-[assembly: AssemblyCopyright("Copyright © SharpEnviro 2009")]
+[assembly: AssemblyProduct("SharpEnviro")]
+[assembly: AssemblyCopyright("SharpEnviro Development Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
