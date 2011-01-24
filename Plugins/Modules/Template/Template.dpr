c8f59c7cfa006c83c8e2ff44ff309c2892f5c042
Mathias Tillman <homersp@sharpe.gov>
2011-01-24 21:43:04 +0000

    * Added working template

---
+++ Plugins/Modules/Template/Template.dpr
@@ -0,0 +1,203 @@
+{
+Source Name: Template.dpr
+Description: SharpBar Template Module - Main Project File
+Copyright (C) Mathias Tillman <mathias@sharpenviro.com>
+
+Source Forge Site
+https://sourceforge.net/projects/sharpe/
+
+SharpE Site
+http://www.sharpenviro.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+library Template;
+
+{$R 'VersionInfo.res'}
+{$R *.res}
+
+uses
+//  VCLFixPack,
+  Windows,
+  Controls,
+  SysUtils,
+  Forms,
+  Classes,
+  Graphics,
+  Contnrs,
+  SharpApi,
+  uISharpBarModule,
+  uISharpESkin,
+  uISharpBar,
+  uInterfacedSharpBarModuleBase,
+  GR32,
+  GR32_PNG,
+  {$IFDEF DEBUG}DebugDialog in '..\..\..\Common\Units\DebugDialog\DebugDialog.pas',{$ENDIF}
+  MainWnd in 'MainWnd.pas' {MainForm};
+
+type
+  TInterfacedSharpBarModule = class(TInterfacedSharpBarModuleBase)
+    private
+    public
+      constructor Create(pID,pBarID : integer; pBarWnd : hwnd); override;
+
+      function CloseModule : HRESULT; override;
+      function SetTopHeight(Top,Height : integer) : HRESULT; override;
+      function UpdateMessage(part : TSU_UPDATE_ENUM; param : integer) : HRESULT; override;
+      function InitModule : HRESULT; override;
+
+      procedure SetSkinInterface(Value : ISharpESkinInterface); override;
+      procedure SetSize(Value : integer); override;
+      procedure SetLeft(Value : integer); override;
+  end;
+
+{ TInterfacedSharpBarModule }
+
+function TInterfacedSharpBarModule.CloseModule: HRESULT;
+begin
+  try
+    Form.Free;
+    Form := nil;
+    result := S_OK;
+  except
+    on E:Exception do
+    begin
+      result := E_FAIL;
+      SharpApi.SendDebugMessageEx(PChar(ModuleName),PChar('Error in CloseModule('
+        + inttostr(ID) + '):' + E.Message),clred,DMT_ERROR);
+    end;
+  end;
+end;
+
+constructor TInterfacedSharpBarModule.Create(pID, pBarID: integer;
+  pBarWnd: hwnd);
+begin
+  inherited Create(pID, pBarID, pBarWnd);
+  ModuleName := 'Template Module';
+
+  try
+    Form := TMainForm.CreateParented(BarWnd);
+    Form.BorderStyle := bsNone;
+    TMainForm(Form).mInterface := self;
+    Form.ParentWindow := BarWnd;
+  except
+    on E:Exception do
+    begin
+      SharpApi.SendDebugMessageEx(PChar(ModuleName),PChar('Error in CreateModule('
+        + inttostr(ID) + '):' + E.Message),clred,DMT_ERROR);
+      exit;
+    end;
+  end;
+end;
+
+function TInterfacedSharpBarModule.InitModule: HRESULT;
+begin
+  result := inherited InitModule;
+
+  if Form <> nil then
+  begin
+    TMainForm(Form).LoadSettings;
+    TMainForm(Form).RealignComponents;         
+  end;
+end;
+
+procedure TInterfacedSharpBarModule.SetLeft(Value: integer);
+begin
+  inherited SetLeft(Value);
+
+  if Form <> nil then
+  begin
+    TMainForm(Form).ReAlignComponents;
+    TMainForm(Form).Repaint;
+  end; 
+end;
+
+procedure TInterfacedSharpBarModule.SetSize(Value: integer);
+begin
+  inherited SetSize(Value);
+
+  TMainForm(Form).UpdateSize;
+end;
+
+procedure TInterfacedSharpBarModule.SetSkinInterface(Value: ISharpESkinInterface);
+begin
+  inherited SetSkinInterface(Value);
+
+  if Form <> nil then
+    TMainForm(Form).UpdateComponentSkins;
+end;
+
+function TInterfacedSharpBarModule.SetTopHeight(Top, Height: integer): HRESULT;
+begin
+  result := inherited SetTopHeight(Top, Height);
+
+  if (Form <> nil) and (Initialized) then
+    TMainForm(Form).RealignComponents(False);
+end;
+
+function TInterfacedSharpBarModule.UpdateMessage(part: TSU_UPDATE_ENUM;
+  param: integer): HRESULT;
+const
+  processed : TSU_UPDATES = [suSkinFileChanged,suBackground,suTheme,suSkin,
+                             suScheme,suIconSet,suSkinFont,suModule];
+begin
+  result := inherited UpdateMessage(part,param);
+
+  if not (Initialized) then
+    exit;
+
+  if not (part in processed) then
+    exit;  
+
+  if (part = suModule) and (ID  = param) then
+  begin
+    TMainForm(Form).LoadSettings;
+    TMainForm(Form).ReAlignComponents;
+  end;
+
+  if [part] <= [suTheme,suSkinFileChanged] then
+    TMainForm(Form).ReAlignComponents;
+
+  if [part] <= [suTheme,suIconSet] then
+    TMainForm(Form).LoadIcons;   
+end;
+
+function GetMetaData(Preview : TBitmap32) : TMetaData;
+begin
+  with result do
+  begin
+    Name := 'Template';
+    Author := 'Mathias Tillman <mathias@sharpenviro.com>';
+    Description := 'Template module';
+    Version := '0.8.0.0';
+    ExtraData := 'preview: False';
+    DataType := tteModule;
+  end;
+end;
+
+function CreateModule(ID,BarID : integer; BarWnd : hwnd) : IInterface;
+begin
+  result := TInterfacedSharpBarModule.Create(ID,BarID,BarWnd);
+end;
+
+
+Exports
+  CreateModule,
+  GetMetaData;
+
+
+begin
+end.
+
